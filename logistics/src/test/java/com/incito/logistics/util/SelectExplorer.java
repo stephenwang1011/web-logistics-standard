3aefa398a18eee22bf2b33becfc6637cff2549d5
wangyang <398733146@qq.com>
2014-08-29 09:49:50 +0800

    选择浏览器添加description

--- logistics/src/test/java/com/incito/logistics/util/SelectExplorer.java
+++ logistics/src/test/java/com/incito/logistics/util/SelectExplorer.java
@@ -1,6 +1,10 @@
 package com.incito.logistics.util;
+/**
+ * @author wangyang
+ * @Description 这是一个选择平台和浏览器的操作
+ * 
+ * */
 import java.util.Properties;
-
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
