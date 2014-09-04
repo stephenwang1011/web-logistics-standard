861b87aa9161092ab88655aab9cab4a2ebb77301
wangyang <398733146@qq.com>
2014-09-04 23:23:02 +0800

    新增：读取属性文件获取测试数据的类

---
+++ logistics/src/test/java/com/incito/logistics/util/GetTestData.java
@@ -0,0 +1,24 @@
+package com.incito.logistics.util;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+public class GetTestData {
+
+	/**
+	 * 获取属性文件的测试数据 
+	 * */
+	public  static String getTestData(String configFilePath ,String key){
+		Configuration config = null;
+		try {
+			config = new PropertiesConfiguration(configFilePath);
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		}
+		return String.valueOf(config.getProperty(key)) ;
+		
+	}
+	}
+
+
