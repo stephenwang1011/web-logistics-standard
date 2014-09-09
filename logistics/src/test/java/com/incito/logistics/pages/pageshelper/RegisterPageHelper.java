e6d74980300d0d79443f0635ada9cd99cf7a0a0c
wangkai <646651700@qq.com>
2014-09-09 21:55:16 +0800

    添加成功登录

--- logistics/src/test/java/com/incito/logistics/pages/pageshelper/RegisterPageHelper.java
+++ logistics/src/test/java/com/incito/logistics/pages/pageshelper/RegisterPageHelper.java
@@ -1,30 +1,35 @@
 package com.incito.logistics.pages.pageshelper;
 
+import org.openqa.selenium.By;
+
 import com.incito.logistics.pages.RegisterPage;
 import com.incito.logistics.util.SeleniumUtil;
 
 public class RegisterPageHelper {
 
-	/**等待页面上某个重要元素显示出来*/
-	public static void  waitRegisterPageToLoad(int timeOut,SeleniumUtil seleniumUtil){
-		seleniumUtil.waitForElementToLoad(timeOut,RegisterPage.RP_INPUT_USERNAME);
-		seleniumUtil.waitForElementToLoad(timeOut,RegisterPage.RP_INPUT_PASSWD);
-		seleniumUtil.waitForElementToLoad(timeOut,RegisterPage.RP_INPUT_REPASSWD);
-		seleniumUtil.waitForElementToLoad(timeOut,RegisterPage.RP_BUTTON_REGISTER);
-		seleniumUtil.waitForElementToLoad(timeOut,RegisterPage.RP_BUTTON_BACK);
+	/** 等待页面上某个重要元素显示出来 */
+	public static void waitRegisterPageToLoad(int timeOut, SeleniumUtil seleniumUtil) {
+		seleniumUtil.waitForElementToLoad(timeOut, RegisterPage.RP_INPUT_USERNAME);
+		seleniumUtil.waitForElementToLoad(timeOut, RegisterPage.RP_INPUT_PASSWD);
+		seleniumUtil.waitForElementToLoad(timeOut, RegisterPage.RP_INPUT_REPASSWD);
+		seleniumUtil.waitForElementToLoad(timeOut, RegisterPage.RP_BUTTON_REGISTER);
+		seleniumUtil.waitForElementToLoad(timeOut, RegisterPage.RP_BUTTON_BACK);
 
 	}
-	
-	/**检查注册页面上的文本*/
-	public static void checkRegisterPageText(SeleniumUtil seleniumUtil){
-		seleniumUtil.isTextEquals( seleniumUtil.getTitle(), "logistics - 物流管理");
-		seleniumUtil.isTextEquals(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_USERNAME).getAttribute("placeholder"),"至少输入4个字符");
-		seleniumUtil.isTextEquals(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_PASSWD).getAttribute("placeholder"),"至少输入6个字符");
-		seleniumUtil.isTextEquals(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_REPASSWD).getAttribute("placeholder"),"请再次输入登录密码");
-		seleniumUtil.isTextEquals(seleniumUtil.findElementBy(RegisterPage.RP_BUTTON_REGISTER).getText(),"注册");
-		seleniumUtil.isTextEquals(seleniumUtil.findElementBy(RegisterPage.RP_BUTTON_BACK).getText(),"返回");
+
+	/** 检查注册页面上的文本 */
+	public static void checkRegisterPageText(SeleniumUtil seleniumUtil) {
+		seleniumUtil.isTextEquals(seleniumUtil.getTitle(), "logistics - 物流管理");
+		seleniumUtil.isTextEquals(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_USERNAME).getAttribute("placeholder"), "至少输入4个字符");
+		seleniumUtil.isTextEquals(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_PASSWD).getAttribute("placeholder"), "至少输入6个字符");
+		seleniumUtil.isTextEquals(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_REPASSWD).getAttribute("placeholder"), "请再次输入登录密码");
+		seleniumUtil.isTextEquals(seleniumUtil.findElementBy(RegisterPage.RP_BUTTON_REGISTER).getText(), "注册");
+		seleniumUtil.isTextEquals(seleniumUtil.findElementBy(RegisterPage.RP_BUTTON_BACK).getText(), "返回");
 		FooterPageHelper.checkRegisterPageText(seleniumUtil);
 	}
-	
 
+	/** 注册页面上面输入用户名和密码 */
+	public static void registerUserInfo(SeleniumUtil seleniumUtil, By by, String key) {
+		seleniumUtil.sendKeys(seleniumUtil.findElementBy(by), key);
+	}
 }
---
+++ logistics/src/test/java/com/incito/logistics/testcase/register/RegisterPage_2_Success_Test.java
@@ -0,0 +1,43 @@
+package com.incito.logistics.testcase.register;
+
+/**
+ *@author  xy-incito
+ *@Description 测试用例：成功注册
+ * */
+import org.openqa.selenium.By;
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.LoginPage;
+import com.incito.logistics.pages.RegisterPage;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.RegisterPageHelper;
+import com.incito.logistics.util.GetTestData;
+
+public class RegisterPage_2_Success_Test extends BaseParpare {
+
+	@Test
+	public void RegisterSuccessTest(ITestContext context) {
+		String configFilePath = context.getCurrentXmlTest().getParameter("userInfoPath");
+		String register_username = GetTestData.getTestData(configFilePath, "register_username");
+		String register_password = GetTestData.getTestData(configFilePath, "register_password");
+		String register_repassword = GetTestData.getTestData(configFilePath, "register_repassword");
+		String keys[] = { register_username, register_password, register_repassword };
+		By bys[] = { RegisterPage.RP_INPUT_USERNAME, RegisterPage.RP_INPUT_PASSWD, RegisterPage.RP_INPUT_REPASSWD };
+
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_REG);
+		RegisterPageHelper.waitRegisterPageToLoad(timeOut, seleniumUtil);
+		RegisterPageHelper.checkRegisterPageText(seleniumUtil);
+		for (int i = 0; i < keys.length; i++) {
+			RegisterPageHelper.registerUserInfo(seleniumUtil, bys[i], keys[i]);
+		}
+		seleniumUtil.click(seleniumUtil.findElementBy(RegisterPage.RP_BUTTON_REGISTER));
+
+	}
+
+}
