6c228bda28ee279ad10ebc50fba6423dee378417
wangkai <646651700@qq.com>
2014-09-11 11:10:20 +0800

    新增注册全部为空和只填写用户名名称

---
+++ logistics/src/test/java/com/incito/logistics/testcase/register/RegisterPage_3_Fail_All_Empty_Test.java
@@ -0,0 +1,30 @@
+package com.incito.logistics.testcase.register;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.RegisterPage;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.RegisterPageHelper;
+
+/**
+ * @author xy-incito
+ * @Description 注册失败：用户名和密码都不填写
+ * */
+public class RegisterPage_3_Fail_All_Empty_Test extends BaseParpare {
+
+	@Test
+	public void registerFailTest_All_Empty(ITestContext context) {
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		
+
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_REG);
+		RegisterPageHelper.waitRegisterPageToLoad(timeOut, seleniumUtil);
+		seleniumUtil.click(seleniumUtil.findElementBy(RegisterPage.RP_BUTTON_REGISTER));
+		RegisterPageHelper.checkRegisterPagePoint(timeOut, seleniumUtil);
+
+	}
+}
---
+++ logistics/src/test/java/com/incito/logistics/testcase/register/RegisterPage_4_Fail_Username_Test.java
@@ -0,0 +1,34 @@
+package com.incito.logistics.testcase.register;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.RegisterPage;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.RegisterPageHelper;
+import com.incito.logistics.util.GetTestData;
+
+/**
+ * @author xy-incito
+ * @Description 注册失败：只输入用户名不输入密码 点击登陆会提示“密码长度为6-20个字符”
+ * */
+public class RegisterPage_4_Fail_Username_Test extends BaseParpare {
+
+	@Test
+	public void registerFailTest_Username(ITestContext context) {
+		String configFilePath = context.getCurrentXmlTest().getParameter("userInfoPath");
+		String register_username = GetTestData.getTestData(configFilePath, "register_username");
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_REG);
+		RegisterPageHelper.waitRegisterPageToLoad(timeOut, seleniumUtil);
+		seleniumUtil.clear(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_USERNAME));
+		seleniumUtil.sendKeys(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_USERNAME), register_username);
+		seleniumUtil.click(seleniumUtil.findElementBy(RegisterPage.RP_BUTTON_REGISTER));
+		RegisterPageHelper.checkRegisterPagePoint(timeOut, seleniumUtil);
+	}
+
+}
