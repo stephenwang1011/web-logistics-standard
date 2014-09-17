acf11df5f98f798022ebc416e28055a9d13009ad
wangyang <398733146@qq.com>
2014-09-17 18:04:41 +0800

    新增完善用户信息页面

---
+++ logistics/src/test/java/com/incito/logistics/pages/AddUserInfoPage.java
@@ -0,0 +1,28 @@
+package com.incito.logistics.pages;
+
+import org.openqa.selenium.By;
+
+/**
+ * 定义完善用户信息页面元素
+ * 
+ * */
+public class AddUserInfoPage {
+	/** 姓名输入框 */
+	public static final By HP_INPUT_NAME = By.name("name");
+	/**联系电话输入框*/
+	public static final By HP_INPUT_TEL = By.name("tel");
+	/**身份证输入框*/
+	public static final By HP_INPUT_COMPANY = By.name("company");
+	/**身份证输入框*/
+	public static final By HP_INPUT_IDCARD = By.name("icard");
+	/**身份证输入框*/
+	public static final By HP_SELECT_PROVINCE = By.id("province");
+	/**身份证输入框*/
+	public static final By HP_INPUT_CITY = By.id("city");
+	/**身份证输入框*/
+	public static final By HP_INPUT_REGION = By.id("region");
+	
+	
+	
+
+}
