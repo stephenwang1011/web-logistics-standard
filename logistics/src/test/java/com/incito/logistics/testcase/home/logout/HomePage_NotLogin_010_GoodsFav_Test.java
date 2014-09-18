56912647a5c95b28470dbeaafef5ac060aa5983d
wangyang <398733146@qq.com>
2014-09-18 17:25:03 +0800

    由于需求改版，重新写了这三个用例

--- logistics/src/test/java/com/incito/logistics/testcase/home/logout/HomePage_NotLogin_010_GoodsFav_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/home/logout/HomePage_NotLogin_010_GoodsFav_Test.java
@@ -1,8 +1,8 @@
 package com.incito.logistics.testcase.home.logout;
 
 /**
- *@author  xy-incito
- *@Description 测试用例：未登录情况下点击公共车源的定位按钮 弹出登陆页面
+ *@author  xy-incito-wy
+ *@Description 测试用例：未登录情况下点击公共货源的收藏按钮 弹出登陆页面
  *
  * */
 import org.testng.ITestContext;
@@ -13,13 +13,14 @@ import com.incito.logistics.pages.HomePage;
 import com.incito.logistics.pages.pageshelper.HomePageHelper;
 import com.incito.logistics.pages.pageshelper.LoginPageHelper;
 
-public class HomePage_NotLogin_011_Loc_Test extends BaseParpare {
+public class HomePage_NotLogin_010_GoodsFav_Test extends BaseParpare {
 	@Test
-	public void clickLoc(ITestContext context) {
+	public void clickFavButton(ITestContext context) {
 		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		int indexGoods = Integer.valueOf(context.getCurrentXmlTest().getParameter("indexGoods"));
+
 		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
-		seleniumUtil.mouseMoveToElement(HomePage.HP_AREAR_CARS1);
-		seleniumUtil.click(seleniumUtil.findElementBy(HomePage.HP_CARS1_LOC));
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_ARERA_CARDSANDGOODS, HomePage.HP_BUTTON_FAV, indexGoods);
 		LoginPageHelper.waitLoginPageToLoad(timeOut, seleniumUtil);
 		LoginPageHelper.checkLoginPageText(seleniumUtil);
 
