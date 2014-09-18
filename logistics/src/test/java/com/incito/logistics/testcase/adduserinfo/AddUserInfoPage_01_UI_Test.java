3252cc7777ac5d2f6019552e20bd22928f1dcb4c
wangyang <398733146@qq.com>
2014-09-18 12:46:54 +0800

    完成了此UI的测试用例

--- logistics/src/test/java/com/incito/logistics/testcase/adduserinfo/AddUserInfoPage_01_UI_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/adduserinfo/AddUserInfoPage_01_UI_Test.java
@@ -1,7 +1,7 @@
 package com.incito.logistics.testcase.adduserinfo;
 import org.testng.ITestContext;
 import org.testng.annotations.Test;
-
+import com.incito.logistics.pages.pageshelper.AddUserInfoPagerHelper;
 import com.incito.logistics.base.BaseParpare;
 import com.incito.logistics.pages.HomePage;
 import com.incito.logistics.pages.pageshelper.HomePageHelper;
@@ -17,13 +17,16 @@ public class AddUserInfoPage_01_UI_Test extends BaseParpare {
 	public void addUserInfoPageUiTest(ITestContext context) {
 		String configFilePath = String.valueOf(context.getCurrentXmlTest().getParameter("userInfoPath"));
 		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
-		String username = PropertiesDataProvider.getTestData(configFilePath, "username");
+		String register_username = PropertiesDataProvider.getTestData(configFilePath, "register_username");
 		String password = PropertiesDataProvider.getTestData(configFilePath, "password");
 
 		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil); 
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
-		LoginPageHelper.login(seleniumUtil, username, password);
+		LoginPageHelper.login(seleniumUtil, register_username, password);
+		seleniumUtil.waitForElementToLoad(timeOut, HomePage.HP_BUTTON_FREESEND);
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_FREESEND);
+		AddUserInfoPagerHelper.waitAddUserInfoPageToLoad(timeOut, seleniumUtil);
+		AddUserInfoPagerHelper.checkAddUserInfoPageText(seleniumUtil);
 
 		
 
