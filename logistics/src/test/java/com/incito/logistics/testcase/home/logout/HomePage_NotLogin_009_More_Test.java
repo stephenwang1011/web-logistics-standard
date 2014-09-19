8a82c4a4b1dc421196d01c378c8f1342232d6214
wangyang <398733146@qq.com>
2014-09-19 11:15:01 +0800

    需求变更 对应更新 用例

--- logistics/src/test/java/com/incito/logistics/testcase/home/logout/HomePage_NotLogin_009_More_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/home/logout/HomePage_NotLogin_009_More_Test.java
@@ -1,8 +1,8 @@
 package com.incito.logistics.testcase.home.logout;
 
 /**
- *@author  xy-incito
- *@Description 测试用例：未登录情况下点击"更多" 弹出登陆页面
+ *@author  xy-incito-wy
+ *@Description 测试用例：未登录情况下点击公共车源和货源上的 "更多>>"  按钮弹出登陆页面
  *
  * */
 import org.testng.ITestContext;
@@ -10,6 +10,7 @@ import org.testng.annotations.Test;
 
 import com.incito.logistics.base.BaseParpare;
 import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.LoginPage;
 import com.incito.logistics.pages.pageshelper.HomePageHelper;
 import com.incito.logistics.pages.pageshelper.LoginPageHelper;
 
@@ -17,10 +18,21 @@ public class HomePage_NotLogin_009_More_Test extends BaseParpare {
 	@Test
 	public void clickMore(ITestContext context) {
 		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		int sleepTime = Integer.valueOf(context.getCurrentXmlTest().getParameter("sleepTime"));
+		int size = seleniumUtil.findElementsBy(HomePage.HP_LINK_MORE).size();
+		
 		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
-		seleniumUtil.click(seleniumUtil.findElementBy(HomePage.HP_LINK_MORE));
-		LoginPageHelper.waitLoginPageToLoad(timeOut, seleniumUtil);
-		LoginPageHelper.checkLoginPageText(seleniumUtil);
+		for (int i = 0; i <size;  i++) {
+			seleniumUtil.click(seleniumUtil.findElementsBy(HomePage.HP_LINK_MORE).get(i));
+			LoginPageHelper.waitLoginPageToLoad(timeOut, seleniumUtil);
+			LoginPageHelper.checkLoginPageText(seleniumUtil);
+			seleniumUtil.click(seleniumUtil.findElementBy(LoginPage.LP_BUTTON_CLOSE));
+			if(i<1){
+			seleniumUtil.pause(sleepTime);
+			}
+		}
+		
+
 
 	}
 
