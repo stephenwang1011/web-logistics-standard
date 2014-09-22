302fb4cbee55ff359128fcb68a1be43b74a55a77
wangkai <646651700@qq.com>
2014-09-22 18:37:37 +0800

    在完善用户信息中添加各种提示类信息

---
+++ logistics/src/test/java/com/incito/logistics/testcase/adduserinfo/AddUserInfoPage_14_Fail_TelAddress_Text.java
@@ -0,0 +1,35 @@
+package com.incito.logistics.testcase.adduserinfo;
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.pages.pageshelper.AddUserInfoPagerHelper;
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.AddUserInfoPage;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.util.PropertiesDataProvider;
+/**
+ *@author  xy-incito-wk
+ *@Description 填写信息联系方式和公司地址为空提交
+ *
+ * */
+public class AddUserInfoPage_14_Fail_TelAddress_Text extends BaseParpare {
+	@Test
+	public void addUserInfoPageFailAllTest(ITestContext context) {
+		String configFilePath = String.valueOf(context.getCurrentXmlTest().getParameter("userInfoPath"));
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		String register_username = PropertiesDataProvider.getTestData(configFilePath, "register_username");
+		String password = PropertiesDataProvider.getTestData(configFilePath, "password");
+
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil); 
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
+		LoginPageHelper.login(seleniumUtil, register_username, password);
+		seleniumUtil.waitForElementToLoad(timeOut, HomePage.HP_BUTTON_FREESEND);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_FREESEND);
+		AddUserInfoPagerHelper.waitAddUserInfoPageToLoad(timeOut, seleniumUtil);
+		AddUserInfoPagerHelper.enterPage(seleniumUtil, AddUserInfoPage.AUIP_BUTTON_SUBMIT);
+		AddUserInfoPagerHelper.checkAddUserInfoPrompt_
+	}
+
+}
