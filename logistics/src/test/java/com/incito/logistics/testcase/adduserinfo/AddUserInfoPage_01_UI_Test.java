46fe77d918a33f441988ba185786effb35f3a878
wangkai <646651700@qq.com>
2014-09-22 18:36:13 +0800

    格式化

--- logistics/src/test/java/com/incito/logistics/testcase/adduserinfo/AddUserInfoPage_01_UI_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/adduserinfo/AddUserInfoPage_01_UI_Test.java
@@ -1,4 +1,5 @@
 package com.incito.logistics.testcase.adduserinfo;
+
 import org.testng.ITestContext;
 import org.testng.annotations.Test;
 import com.incito.logistics.pages.pageshelper.AddUserInfoPagerHelper;
@@ -7,9 +8,10 @@ import com.incito.logistics.pages.HomePage;
 import com.incito.logistics.pages.pageshelper.HomePageHelper;
 import com.incito.logistics.pages.pageshelper.LoginPageHelper;
 import com.incito.logistics.util.PropertiesDataProvider;
+
 /**
- *@author  xy-incito-wy
- *@Description 完善信息页面上的UI检查
+ * @author xy-incito-wy
+ * @Description 完善信息页面上的UI检查
  *
  * */
 public class AddUserInfoPage_01_UI_Test extends BaseParpare {
@@ -20,15 +22,13 @@ public class AddUserInfoPage_01_UI_Test extends BaseParpare {
 		String register_username = PropertiesDataProvider.getTestData(configFilePath, "register_username");
 		String password = PropertiesDataProvider.getTestData(configFilePath, "password");
 
-		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil); 
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
 		LoginPageHelper.login(seleniumUtil, register_username, password);
 		seleniumUtil.waitForElementToLoad(timeOut, HomePage.HP_BUTTON_FREESEND);
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_FREESEND);
 		AddUserInfoPagerHelper.waitAddUserInfoPageToLoad(timeOut, seleniumUtil);
 		AddUserInfoPagerHelper.checkAddUserInfoPageText(seleniumUtil);
-
-		
 
 	}
 
