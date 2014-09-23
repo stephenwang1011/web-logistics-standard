ea3a4fa04614b497d7970edf6d588de9c3b8f040
wangkai <646651700@qq.com>
2014-09-23 18:08:49 +0800

    完成空白点击提交测试用例

---
+++ logistics/src/test/java/com/incito/logistics/testcase/adduserinfo/AddUserInfoPage_31_Fail_NameIdcardCompanyAddress_Text.java
@@ -0,0 +1,37 @@
+package com.incito.logistics.testcase.adduserinfo;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.pages.pageshelper.AddUserInfoPagerHelper;
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.AddUserInfoPage;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.util.PropertiesDataProvider;
+
+/**
+ * @author xy-incito-wk
+ * @Description 填写姓名，身份证号，公司名称，公司地址点击提交
+ *
+ * */
+public class AddUserInfoPage_31_Fail_NameIdcardCompanyAddress_Text extends BaseParpare {
+	@Test
+	public void addUserInfoPageFailNameIdcardCompanyAddress(ITestContext context) {
+		String configFilePath = String.valueOf(context.getCurrentXmlTest().getParameter("userInfoPath"));
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		String register_username = PropertiesDataProvider.getTestData(configFilePath, "register_username");
+		String password = PropertiesDataProvider.getTestData(configFilePath, "password");
+
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
+		LoginPageHelper.login(seleniumUtil, register_username, password);
+		seleniumUtil.waitForElementToLoad(timeOut, HomePage.HP_BUTTON_FREESEND);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_FREESEND);
+		AddUserInfoPagerHelper.waitAddUserInfoPageToLoad(timeOut, seleniumUtil);
+		AddUserInfoPagerHelper.enterPage(seleniumUtil, AddUserInfoPage.AUIP_BUTTON_SUBMIT);
+		AddUserInfoPagerHelper.checkAddUserInfoPrompt_NameIdcardCompanyAddress(timeOut, seleniumUtil);
+	}
+
+}
