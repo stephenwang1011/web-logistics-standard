13c7599b76838c69edf1acf98bc3b9e616bc3a6c
wangyang <398733146@qq.com>
2014-09-27 10:25:29 +0800

    修改了 footerpage的checkFooterPageText()方法

--- logistics/src/test/java/com/incito/logistics/pages/pageshelper/RegisterPageHelper.java
+++ logistics/src/test/java/com/incito/logistics/pages/pageshelper/RegisterPageHelper.java
@@ -30,7 +30,7 @@ public class RegisterPageHelper {
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_REPASSWD).getAttribute("placeholder"), "请再次输入登录密码");
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_BUTTON_REGISTER).getText(), "注册");
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_BUTTON_BACK).getText(), "返回");
-		FooterPageHelper.checkRegisterPageText(seleniumUtil);
+		FooterPageHelper.checkFooterPageText(seleniumUtil);
 	}
 
 	/** 注册页面上面输入用户名和密码 */
@@ -63,18 +63,18 @@ public class RegisterPageHelper {
 	public static void checkRegisterPagePrompt(int timeOut, SeleniumUtil seleniumUtil) {
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_USERNAME_P).getText(), "用户名称不能全部为数字");
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_PASSWD_P).getText(), "密码长度为6-20个字符");
-		FooterPageHelper.checkRegisterPageText(seleniumUtil);
+		FooterPageHelper.checkFooterPageText(seleniumUtil);
 	}
 
 	/** 检查注册页面上只输入“用户名称”的的提示语 */
 	public static void checkRegisterPagePrompt_username(int timeOut, SeleniumUtil seleniumUtil) {
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_PASSWD_P).getText(), "密码长度为6-20个字符");
-		FooterPageHelper.checkRegisterPageText(seleniumUtil);
+		FooterPageHelper.checkFooterPageText(seleniumUtil);
 	}
 	/** 检查注册页面上的只输入“密码1”的提示语 */
 	public static void checkRegisterPagePrompt_passwd(int timeOut, SeleniumUtil seleniumUtil) {
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_USERNAME_P).getText(), "用户名称不能全部为数字");
-		FooterPageHelper.checkRegisterPageText(seleniumUtil);
+		FooterPageHelper.checkFooterPageText(seleniumUtil);
 	}
 	
 	/** 检查注册页面上的只输入“密码2”的提示语 */
@@ -82,7 +82,7 @@ public class RegisterPageHelper {
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_USERNAME_P).getText(), "用户名称不能全部为数字");
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_PASSWD_P).getText(), "密码长度为6-20个字符");
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_REPASSWD_P).getText(), "两次密码输入不一致，请重新输入");
-		FooterPageHelper.checkRegisterPageText(seleniumUtil);
+		FooterPageHelper.checkFooterPageText(seleniumUtil);
 	}
 
 	/** 检查注册页面上的输入“用户名”和“密码1”的提示语 */
@@ -91,32 +91,32 @@ public class RegisterPageHelper {
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_USERNAME_P).getText(), "用户名称不能全部为数字");
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_PASSWD_P).getText(), "密码长度为6-20个字符");
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_REPASSWD_P).getText(), "两次密码输入不一致，请重新输入");
-		FooterPageHelper.checkRegisterPageText(seleniumUtil);
+		FooterPageHelper.checkFooterPageText(seleniumUtil);
 	}
 	
 	/** 检查注册页面上的输入“用户名”和“密码2”的提示语 */
 	public static void checkRegisterPagePrompt_UsernameAndRepassword(int timeOut, SeleniumUtil seleniumUtil) {
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_PASSWD_P).getText(), "密码长度为6-20个字符");
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_REPASSWD_P).getText(), "两次密码输入不一致，请重新输入");
-		FooterPageHelper.checkRegisterPageText(seleniumUtil);
+		FooterPageHelper.checkFooterPageText(seleniumUtil);
 	}
 	
 	/** 检查注册页面上的输入相同“密码1”和“密码2”的提示语 */
 	public static void checkRegisterPagePrompt_PasswordAndRepassword(int timeOut, SeleniumUtil seleniumUtil) {
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_USERNAME_P).getText(), "用户名称不能全部为数字");
-		FooterPageHelper.checkRegisterPageText(seleniumUtil);
+		FooterPageHelper.checkFooterPageText(seleniumUtil);
 	}
 	
 	/** 检查注册页面上的输入不相同“密码1”和“密码2”的提示语 */
 	public static void checkRegisterPagePrompt_PasswordDifRepassword(int timeOut, SeleniumUtil seleniumUtil) {
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_USERNAME_P).getText(), "用户名称不能全部为数字");
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_REPASSWD_P).getText(), "两次密码输入不一致，请重新输入");
-		FooterPageHelper.checkRegisterPageText(seleniumUtil);
+		FooterPageHelper.checkFooterPageText(seleniumUtil);
 	}
 	
 	/** 检查注册页面上的输入“用户名称”与不相同“密码1”和“密码2”的提示语 */
 	public static void checkRegisterPagePrompt_UssernameAndPasswordDifRepassword(int timeOut, SeleniumUtil seleniumUtil) {
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_REPASSWD_P).getText(), "两次密码输入不一致，请重新输入");
-		FooterPageHelper.checkRegisterPageText(seleniumUtil);
+		FooterPageHelper.checkFooterPageText(seleniumUtil);
 	}
 }
