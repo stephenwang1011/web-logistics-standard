101ea70eb7eafdda2d3e0a460b04bb817f615869
wangyang <398733146@qq.com>
2014-10-09 15:03:06 +0800

    excel做数据存储的例子

--- logistics/src/test/java/com/incito/logistics/testcase/login/LoginPage_000_Password_Empty_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/login/LoginPage_000_Password_Empty_Test.java
@@ -19,28 +19,41 @@ import com.incito.logistics.util.ExcelDataProvider;
  * @author xy-incito
  * @Description 登陆失败：用户名和密码都不填写
  * */
-public class Fail_Password_Empty_Test extends BaseParpare {
+public class LoginPage_000_Password_Empty_Test extends BaseParpare {
 
-	@Test(dataProvider = "data" )
-	public void loginFailTest_Password_Empty(ITestContext context,Map<String,String> data) {
+	@Test(dataProvider = "data")
+	public void loginFailTest_Password_Empty(ITestContext context, Map<String, String> data) {
 		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
 		int waitMillisecondsForAlert = Integer.valueOf(context.getCurrentXmlTest().getParameter("waitMillisecondsForAlert"));
 
 		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
 		LoginPageHelper.waitLoginPageToLoad(timeOut, seleniumUtil);
-		
+
 		seleniumUtil.findElementBy(LoginPage.LP_INPUT_USERNAME).sendKeys(data.get("USERNAME"));
 		seleniumUtil.findElementBy(LoginPage.LP_INPUT_USERNAME).sendKeys(data.get("PASSWORD"));
 		seleniumUtil.click(seleniumUtil.findElementBy(LoginPage.LP_BUTTON_LOGIN));
 		seleniumUtil.isTextCorrect(seleniumUtil.switchToPromptedAlertAfterWait(waitMillisecondsForAlert).getText(), data.get("EXPECT"));
 
 	}
+
+	@DataProvider(name = "data")
+	public  Iterator<Object[]> dataFortestMethod() throws IOException {
+		String moduleName = null; //模块的名字
+		String caseNum = null;  //用例编号
+		String className = this.getClass().getName();
+		 int dotIndexNum = className.indexOf("."); //取得第一个.的index 
+		 int underlineIndexNum = className.indexOf("_");  //取得第一个_的index 
+		 
+		 if (dotIndexNum > 0) {
+			 moduleName = className.substring(30,className.lastIndexOf(".")); //取到模块的名称
+		 }
+		 
+		 if (underlineIndexNum > 0) {
+			 caseNum = className.substring(underlineIndexNum+1, underlineIndexNum+4); //取到用例编号
+		 }
+		//将模块名称和用例的编号传给 ExcelDataProvider ，然后进行读取excel数据
+		return new ExcelDataProvider(moduleName, caseNum);
+	}
 	
-	
-    @DataProvider(name = "data")
-    public Iterator<Object[]> dataFortestMethod() throws IOException {
-        return new ExcelDataProvider(this.getClass().getName(),"login");
-    }
-   
 }
