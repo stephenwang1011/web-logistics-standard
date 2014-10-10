540dbaff080d7bbe5de0fab54306917e5c361914
wangyang <398733146@qq.com>
2014-10-10 10:43:32 +0800

    改用从excel中读取数据

--- logistics/src/test/java/com/incito/logistics/testcase/register/RegisterPage_003_Fail_Username_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/register/RegisterPage_003_Fail_Username_Test.java
@@ -1,7 +1,12 @@
 package com.incito.logistics.testcase.register;
 
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.testng.ITestContext;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.incito.logistics.base.BaseParpare;
@@ -9,16 +14,17 @@ import com.incito.logistics.pages.HomePage;
 import com.incito.logistics.pages.RegisterPage;
 import com.incito.logistics.pages.pageshelper.HomePageHelper;
 import com.incito.logistics.pages.pageshelper.RegisterPageHelper;
+import com.incito.logistics.util.ExcelDataProvider;
 import com.incito.logistics.util.PropertiesDataProvider;
 
 /**
- * @author xy-incito-wangkai
+ * @author xy-incito-wk
  * @Description 注册失败：只输入用户名不输入密码 点击登陆会提示“密码长度为6-20个字符”
  * */
 public class RegisterPage_003_Fail_Username_Test extends BaseParpare {
 
-	@Test
-	public void registerFailTest_Username(ITestContext context) {
+	@Test(dataProvider = "data")
+	public void registerFailTest_Username(ITestContext context,Map<String,String> data) {
 		String configFilePath = context.getCurrentXmlTest().getParameter("userInfoPath");
 		String register_username = PropertiesDataProvider.getTestData(configFilePath, "register_username");
 		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
@@ -27,7 +33,7 @@ public class RegisterPage_003_Fail_Username_Test extends BaseParpare {
 		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_REG);
 		RegisterPageHelper.waitRegisterPageToLoad(timeOut, seleniumUtil);
-		RegisterPageHelper.checkRegisterPageText(seleniumUtil);
+		RegisterPageHelper.checkRegisterPageText(seleniumUtil,data);
 		for (By by : bys) {
 			seleniumUtil.clear(seleniumUtil.findElementBy(by));
 		}
@@ -35,5 +41,22 @@ public class RegisterPage_003_Fail_Username_Test extends BaseParpare {
 		RegisterPageHelper.enterPage(seleniumUtil, RegisterPage.RP_BUTTON_REGISTER);
 		RegisterPageHelper.checkRegisterPagePrompt_username(timeOut, seleniumUtil);
 	}
-
+	@DataProvider(name = "data")
+	public  Iterator<Object[]> dataFortestMethod() throws IOException {
+		String moduleName = null; //模块的名字
+		String caseNum = null;  //用例编号
+		String className = this.getClass().getName();
+		 int dotIndexNum = className.indexOf("."); //取得第一个.的index 
+		 int underlineIndexNum = className.indexOf("_");  //取得第一个_的index 
+		 
+		 if (dotIndexNum > 0) {
+			 moduleName = className.substring(30,className.lastIndexOf(".")); //取到模块的名称
+		 }
+		 
+		 if (underlineIndexNum > 0) {
+			 caseNum = className.substring(underlineIndexNum+1, underlineIndexNum+4); //取到用例编号
+		 }
+		//将模块名称和用例的编号传给 ExcelDataProvider ，然后进行读取excel数据
+		return new ExcelDataProvider(moduleName, caseNum);
+	}
 }
--- logistics/src/test/java/com/incito/logistics/testcase/register/RegisterPage_011_End_Success_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/register/RegisterPage_011_End_Success_Test.java
@@ -4,8 +4,13 @@ package com.incito.logistics.testcase.register;
  *@author  xy-incito
  *@Description 测试用例：成功注册
  * */
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.testng.ITestContext;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.incito.logistics.base.BaseParpare;
@@ -13,12 +18,13 @@ import com.incito.logistics.pages.HomePage;
 import com.incito.logistics.pages.RegisterPage;
 import com.incito.logistics.pages.pageshelper.HomePageHelper;
 import com.incito.logistics.pages.pageshelper.RegisterPageHelper;
+import com.incito.logistics.util.ExcelDataProvider;
 import com.incito.logistics.util.PropertiesDataProvider;
 
 public class RegisterPage_011_End_Success_Test extends BaseParpare {
 
-	@Test
-	public void RegisterSuccessTest(ITestContext context) {
+	@Test(dataProvider="data")
+	public void RegisterSuccessTest(ITestContext context,Map<String,String> data) {
 		String configFilePath = context.getCurrentXmlTest().getParameter("userInfoPath");
 		String register_username = PropertiesDataProvider.getTestData(configFilePath, "register_username");
 		String register_password = PropertiesDataProvider.getTestData(configFilePath, "register_password");
@@ -32,7 +38,7 @@ public class RegisterPage_011_End_Success_Test extends BaseParpare {
 		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_REG);
 		RegisterPageHelper.waitRegisterPageToLoad(timeOut, seleniumUtil);
-		RegisterPageHelper.checkRegisterPageText(seleniumUtil);
+		RegisterPageHelper.checkRegisterPageText(seleniumUtil,data);
 		for (By by : bys) {
 			seleniumUtil.clear(seleniumUtil.findElementBy(by));
 		}
@@ -44,4 +50,23 @@ public class RegisterPage_011_End_Success_Test extends BaseParpare {
 
 	}
 
+	@DataProvider(name = "data")
+	public  Iterator<Object[]> dataFortestMethod() throws IOException {
+		String moduleName = null; //模块的名字
+		String caseNum = null;  //用例编号
+		String className = this.getClass().getName();
+		 int dotIndexNum = className.indexOf("."); //取得第一个.的index 
+		 int underlineIndexNum = className.indexOf("_");  //取得第一个_的index 
+		 
+		 if (dotIndexNum > 0) {
+			 moduleName = className.substring(30,className.lastIndexOf(".")); //取到模块的名称
+		 }
+		 
+		 if (underlineIndexNum > 0) {
+			 caseNum = className.substring(underlineIndexNum+1, underlineIndexNum+4); //取到用例编号
+		 }
+		//将模块名称和用例的编号传给 ExcelDataProvider ，然后进行读取excel数据
+		return new ExcelDataProvider(moduleName, caseNum);
+	}
+	
 }
