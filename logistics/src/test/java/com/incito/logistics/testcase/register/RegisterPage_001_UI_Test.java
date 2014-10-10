014de92e6653fe3476e0a49ac363c26d3c87e54a
wangyang <398733146@qq.com>
2014-10-10 10:30:39 +0800

    修改：从excel表中读取测试数据

--- logistics/src/test/java/com/incito/logistics/testcase/register/RegisterPage_001_UI_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/register/RegisterPage_001_UI_Test.java
@@ -1,28 +1,53 @@
 package com.incito.logistics.testcase.register;
 
 /**
- *@author  xy-incito
+ *@author  xy-incito-wy
  *@Description 注册页面上的文本检查
  *
  * */
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
 import org.testng.ITestContext;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.incito.logistics.base.BaseParpare;
 import com.incito.logistics.pages.HomePage;
 import com.incito.logistics.pages.pageshelper.HomePageHelper;
 import com.incito.logistics.pages.pageshelper.RegisterPageHelper;
+import com.incito.logistics.util.ExcelDataProvider;
 
 public class RegisterPage_001_UI_Test extends BaseParpare {
-	@Test
-	public void registerPageUiTest(ITestContext context) {
+	@Test(dataProvider = "data")
+	public void registerPageUiTest(ITestContext context,Map<String, String> data) {
 		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
 
 		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_REG);
 		RegisterPageHelper.waitRegisterPageToLoad(timeOut, seleniumUtil);
-		RegisterPageHelper.checkRegisterPageText(seleniumUtil);
+		RegisterPageHelper.checkRegisterPageText(seleniumUtil,data);
 
+	}
+	
+	@DataProvider(name = "data")
+	public  Iterator<Object[]> dataFortestMethod() throws IOException {
+		String moduleName = null; //模块的名字
+		String caseNum = null;  //用例编号
+		String className = this.getClass().getName();
+		 int dotIndexNum = className.indexOf("."); //取得第一个.的index 
+		 int underlineIndexNum = className.indexOf("_");  //取得第一个_的index 
+		 
+		 if (dotIndexNum > 0) {
+			 moduleName = className.substring(30,className.lastIndexOf(".")); //取到模块的名称
+		 }
+		 
+		 if (underlineIndexNum > 0) {
+			 caseNum = className.substring(underlineIndexNum+1, underlineIndexNum+4); //取到用例编号
+		 }
+		//将模块名称和用例的编号传给 ExcelDataProvider ，然后进行读取excel数据
+		return new ExcelDataProvider(moduleName, caseNum);
 	}
 
 }
