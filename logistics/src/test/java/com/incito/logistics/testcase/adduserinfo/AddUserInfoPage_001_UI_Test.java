c61686ced6e3eeafa23097556d5bae9249c2cd7e
wangkai <646651700@qq.com>
2014-10-11 20:29:21 +0800

    更新完善用户信息，使用execl导入数据

---
+++ logistics/src/test/java/com/incito/logistics/testcase/adduserinfo/AddUserInfoPage_001_UI_Test.java
@@ -0,0 +1,35 @@
+package com.incito.logistics.testcase.adduserinfo;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+import com.incito.logistics.pages.pageshelper.AddUserInfoPagerHelper;
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.util.PropertiesDataProvider;
+
+/**
+ * @author xy-incito-wy
+ * @Description 完善信息页面上的UI检查
+ *
+ * */
+public class AddUserInfoPage_001_UI_Test extends BaseParpare {
+	@Test
+	public void addUserInfoPageUiTest(ITestContext context) {
+		String configFilePath = String.valueOf(context.getCurrentXmlTest().getParameter("userInfoPath"));
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		String registered_username = PropertiesDataProvider.getTestData(configFilePath, "registered_username");
+		String registered_password = PropertiesDataProvider.getTestData(configFilePath, "registered_password");
+
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
+		LoginPageHelper.login(seleniumUtil, registered_username, registered_password);
+		seleniumUtil.waitForElementToLoad(timeOut, HomePage.HP_BUTTON_FREESEND);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_FREESEND);
+		AddUserInfoPagerHelper.waitAddUserInfoPageToLoad(timeOut, seleniumUtil);
+		AddUserInfoPagerHelper.checkAddUserInfoPageText(seleniumUtil);
+
+	}
+
+}
---
+++ logistics/src/test/java/com/incito/logistics/testcase/adduserinfo/AddUserInfoPage_032_Fail_TelIdcardCompanyAddress_Test.java
@@ -0,0 +1,74 @@
+package com.incito.logistics.testcase.adduserinfo;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.testng.ITestContext;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.AddUserInfoPage;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.pageshelper.AddUserInfoPagerHelper;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.util.ExcelDataProvider;
+import com.incito.logistics.util.PropertiesDataProvider;
+
+/**
+ * @author xy-incito-wk
+ * @Description 填写电话，身份证号，公司名称，公司地址点击提交
+ *
+ * */
+public class AddUserInfoPage_032_Fail_TelIdcardCompanyAddress_Test extends BaseParpare {
+	@Test(dataProvider = "data")
+	public void addUserInfoPageFailTelIdcardCompanyAddress(ITestContext context, Map<String, String> data) {
+		String configFilePath = String.valueOf(context.getCurrentXmlTest().getParameter("userInfoPath"));
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		int sleepTime = Integer.valueOf(context.getCurrentXmlTest().getParameter("sleepTime"));
+		String registered_username = PropertiesDataProvider.getTestData(configFilePath, "registered_username");
+		String registered_password = PropertiesDataProvider.getTestData(configFilePath, "registered_password");
+		By[] bys = { AddUserInfoPage.AUIP_INPUT_NAME, AddUserInfoPage.AUIP_INPUT_TEL, AddUserInfoPage.AUIP_INPUT_COMPANY, AddUserInfoPage.AUIP_INPUT_IDCARD, AddUserInfoPage.AUIP_INPUT_ADDRESS };
+
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
+		LoginPageHelper.login(seleniumUtil, registered_username, registered_password);
+		seleniumUtil.waitForElementToLoad(timeOut, HomePage.HP_BUTTON_FREESEND);
+		HomePageHelper.holdOn(seleniumUtil, sleepTime);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_FREESEND);
+		AddUserInfoPagerHelper.waitAddUserInfoPageToLoad(timeOut, seleniumUtil);
+		for (By by : bys) {
+			seleniumUtil.clear(seleniumUtil.findElementBy(by));
+		}
+		seleniumUtil.selectByIndex(AddUserInfoPage.AUIP_SELECT_PROVINCE, 0);
+
+		seleniumUtil.type(seleniumUtil.findElementBy(AddUserInfoPage.AUIP_INPUT_TEL), data.get("ADD_TEL"));
+		seleniumUtil.type(seleniumUtil.findElementBy(AddUserInfoPage.AUIP_INPUT_IDCARD), data.get("ADD_IDCARD"));
+		seleniumUtil.type(seleniumUtil.findElementBy(AddUserInfoPage.AUIP_INPUT_COMPANY), data.get("ADD_COMPANY"));
+		seleniumUtil.address(AddUserInfoPage.AUIP_SELECT_PROVINCE, data.get("ADD_PROVINCE"), AddUserInfoPage.AUIP_SELECT_CITY, data.get("ADD_CITY"), AddUserInfoPage.AUIP_SELECT_REGION, data.get("ADD_REGION"), AddUserInfoPage.AUIP_INPUT_ADDRESS, data.get("ADD_ADDRESS"));
+		AddUserInfoPagerHelper.enterPage(seleniumUtil, AddUserInfoPage.AUIP_BUTTON_SUBMIT);
+		AddUserInfoPagerHelper.checkAddUserInfoPrompt(timeOut, seleniumUtil, data);
+	}
+
+	@DataProvider(name = "data")
+	public Iterator<Object[]> dataFortestMethod() throws IOException {
+		String moduleName = null; // 模块的名字
+		String caseNum = null; // 用例编号
+		String className = this.getClass().getName();
+		int dotIndexNum = className.indexOf("."); // 取得第一个.的index
+		int underlineIndexNum = className.indexOf("_"); // 取得第一个_的index
+
+		if (dotIndexNum > 0) {
+			moduleName = className.substring(30, className.lastIndexOf(".")); // 取到模块的名称
+		}
+
+		if (underlineIndexNum > 0) {
+			caseNum = className.substring(underlineIndexNum + 1, underlineIndexNum + 4); // 取到用例编号
+		}
+		// 将模块名称和用例的编号传给 ExcelDataProvider ，然后进行读取excel数据
+		return new ExcelDataProvider(moduleName, caseNum);
+	}
+}
