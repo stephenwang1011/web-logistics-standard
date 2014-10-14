2b56370f8c61f3552171524640061d237f30f33e
wangyang <398733146@qq.com>
2014-10-14 18:00:47 +0800

    完成了该用例

--- logistics/src/test/java/com/incito/logistics/testcase/sendgoods/SendGoodsPage_005_All_Empty_Data_Fail_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/sendgoods/SendGoodsPage_005_All_Empty_Data_Fail_Test.java
@@ -1,19 +1,14 @@
 package com.incito.logistics.testcase.sendgoods;
 
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
 import org.testng.ITestContext;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
 import com.incito.logistics.base.BaseParpare;
 import com.incito.logistics.pages.HomePage;
 import com.incito.logistics.pages.SendGoodsPage;
 import com.incito.logistics.pages.pageshelper.HomePageHelper;
 import com.incito.logistics.pages.pageshelper.LoginPageHelper;
 import com.incito.logistics.pages.pageshelper.SendGoodsPageHelper;
-import com.incito.logistics.pages.pageshelper.SendGoodsSuccessPageHelper;
-import com.incito.logistics.util.ExcelDataProvider;
 import com.incito.logistics.util.PropertiesDataProvider;
 
 
@@ -23,8 +18,8 @@ import com.incito.logistics.util.PropertiesDataProvider;
  * */
 public class SendGoodsPage_005_All_Empty_Data_Fail_Test extends BaseParpare {
 
-	@Test(dataProvider = "data")
-	public void sendAllEmptyDataFailTest(ITestContext context, Map<String, String> data) {
+	@Test
+	public void sendAllEmptyDataFailTest(ITestContext context) {
 		String userInfoPath = context.getCurrentXmlTest().getParameter("userInfoPath");
 		String username = PropertiesDataProvider.getTestData(userInfoPath, "username");
 		String password = PropertiesDataProvider.getTestData(userInfoPath, "password");
@@ -39,36 +34,8 @@ public class SendGoodsPage_005_All_Empty_Data_Fail_Test extends BaseParpare {
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_FREESEND);
 		SendGoodsPageHelper.waitSendGoodsPageToLoad(timeOut, seleniumUtil);
 		SendGoodsPageHelper.checkSendGoodsPageText(seleniumUtil);
-		SendGoodsPageHelper.typeGoodsInfo(seleniumUtil, SendGoodsPage.SGP_BUTTON_DEVICEGOODS, SendGoodsPage.SGP_BUTTON_GOODSDATE3, 
-				data.get("SGP_INPUT_GOODSORIGINALCITY"), data.get("SGP_INPUT_GOODSRECEIPTCITY"), data.get("SGP_INPUT_GOODSNAME"), data.get("SGP_INPUT_GOODSDETAILS"),
-				data.get("SGP_INPUT_WEIGHT"), data.get("SGP_INPUT_COUNT"), data.get("SGP_INPUT_CARLENGTH"), data.get("SGP_INPUT_CARTYPE"), 
-				data.get("SGP_INPUT_INFOFARE"), data.get("SGP_INPUT_FARE"), data.get("SGP_INPUT_DECLAREVALUE"), data.get("SGP_INPUT_INSTRUCTION"));
 		SendGoodsPageHelper.enterPage(seleniumUtil, SendGoodsPage.SGP_BUTTON_SEND);
-		SendGoodsSuccessPageHelper.waitSendGoodsSuccessPageToLoad(timeOut, seleniumUtil);
-		SendGoodsSuccessPageHelper.checkSendGoodsPageText(seleniumUtil);
-		
-		
+		SendGoodsPageHelper.checkSendGoodsFailedStatus(seleniumUtil);
 		
 	}
-	
-
-	@DataProvider(name = "data")
-	public Iterator<Object[]> dataFortestMethod() throws IOException {
-		String moduleName = null; // 模块的名字
-		String caseNum = null; // 用例编号
-		String className = this.getClass().getName();
-		int dotIndexNum = className.indexOf("."); // 取得第一个.的index
-		int underlineIndexNum = className.indexOf("_"); // 取得第一个_的index
-
-		if (dotIndexNum > 0) {
-			moduleName = className.substring(30, className.lastIndexOf(".")); // 取到模块的名称
-		}
-
-		if (underlineIndexNum > 0) {
-			caseNum = className.substring(underlineIndexNum + 1, underlineIndexNum + 4); // 取到用例编号
-		}
-		// 将模块名称和用例的编号传给 ExcelDataProvider ，然后进行读取excel数据
-		return new ExcelDataProvider(moduleName, caseNum);
-	}
-
 }
