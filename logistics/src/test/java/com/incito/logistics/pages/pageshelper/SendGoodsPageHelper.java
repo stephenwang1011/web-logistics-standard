37d0ae1fda463ba28de5c158459a22d5df486bd6
wangyang <398733146@qq.com>
2014-10-15 17:57:39 +0800

    新增检查发布失败的方法-判断依据：发布按钮和清空按钮 还存在

--- logistics/src/test/java/com/incito/logistics/pages/pageshelper/SendGoodsPageHelper.java
+++ logistics/src/test/java/com/incito/logistics/pages/pageshelper/SendGoodsPageHelper.java
@@ -12,7 +12,7 @@ public class SendGoodsPageHelper {
 
 	/** 等待发布货源页面上某个重要元素显示出来 */
 	public static void waitSendGoodsPageToLoad(int timeOut, SeleniumUtil seleniumUtil) {
-		
+
 		logger.info("Start checking SendGoods Page elements");
 		seleniumUtil.waitForElementToLoad(timeOut, SendGoodsPage.SGP_INPUT_GOODSORIGINALCITY);
 		seleniumUtil.waitForElementToLoad(timeOut, SendGoodsPage.SGP_INPUT_GOODSRECEIPTCITY);
@@ -40,88 +40,100 @@ public class SendGoodsPageHelper {
 	/** 检查发布货源页面页面上的文本 */
 	public static void checkSendGoodsPageText(SeleniumUtil seleniumUtil) {
 		logger.info("Starting checking sendgoods page text");
-		//seleniumUtil.isTextCorrect(seleniumUtil.getTitle(), "logistics - 物流管理");
+		// seleniumUtil.isTextCorrect(seleniumUtil.getTitle(),
+		// "logistics - 物流管理");
 
 		FooterPageHelper.checkFooterPageText(seleniumUtil);
 		logger.info("Checking sendgoods page text completed");
 	}
 
-	/**在发布货源界面上输入相关信息<br>
+	/**
+	 * 在发布货源界面上输入相关信息<br>
 	 * 
 	 * 
 	 * 
 	 * */
-	public static void typeGoodsInfo(SeleniumUtil seleniumUtil,By goodsType,By goodsDate,String... info){
+	public static void typeGoodsInfo(SeleniumUtil seleniumUtil, By goodsType, By goodsDate, String... info) {
 		// 填充发货地
-		String jsFrom = "document.getElementsByName('originalcity')[0].setAttribute('value','"+info[0].toString() + "');";
-		((JavascriptExecutor)seleniumUtil.driver).executeScript(jsFrom);
+		String jsFrom = "document.getElementsByName('originalcity')[0].setAttribute('value','" + info[0].toString() + "');";
+		((JavascriptExecutor) seleniumUtil.driver).executeScript(jsFrom);
 		// 填充收货地
-		String jsTo = "document.getElementsByName('receiptcity')[0].setAttribute('value','"+ info[1].toString() + "');";
+		String jsTo = "document.getElementsByName('receiptcity')[0].setAttribute('value','" + info[1].toString() + "');";
 		((JavascriptExecutor) seleniumUtil.driver).executeScript(jsTo);
-		//选择货物类型
-		if(seleniumUtil.findElementBy(goodsType).isSelected()==false){
+		// 选择货物类型
+		if (seleniumUtil.findElementBy(goodsType).isSelected() == false) {
 			seleniumUtil.click(seleniumUtil.findElementBy(goodsType));
 		}
-		//货物名称
-		String goodsName = "document.getElementsByName('goodsname')[0].setAttribute('value','"+ info[2].toString() + "');";
+		// 货物名称
+		String goodsName = "document.getElementsByName('goodsname')[0].setAttribute('value','" + info[2].toString() + "');";
 		((JavascriptExecutor) seleniumUtil.driver).executeScript(goodsName);
-		//货物详细名称
+		// 货物详细名称
 		seleniumUtil.type(seleniumUtil.findElementBy(SendGoodsPage.SGP_INPUT_GOODSDETAILS), info[3].toString());
-		//货物重量 or 体积
-		if(seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_DEVICEGOODS).isSelected()||seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_HEAVYGOODS).isSelected()){
-		seleniumUtil.type(seleniumUtil.findElementBy(SendGoodsPage.SGP_INPUT_WEIGHT), info[4].toString());
-		}else if(seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_LIGHTGOODS).isSelected()){
-			seleniumUtil.type(seleniumUtil.findElementBy(SendGoodsPage.SGP_INPUT_VOLUME), info[4].toString());	
+		// 货物重量 or 体积
+		if (seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_DEVICEGOODS).isSelected() || seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_HEAVYGOODS).isSelected()) {
+			seleniumUtil.type(seleniumUtil.findElementBy(SendGoodsPage.SGP_INPUT_WEIGHT), info[4].toString());
+		} else if (seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_LIGHTGOODS).isSelected()) {
+			seleniumUtil.type(seleniumUtil.findElementBy(SendGoodsPage.SGP_INPUT_VOLUME), info[4].toString());
 		}
-		//货物件数
+		// 货物件数
 		seleniumUtil.type(seleniumUtil.findElementBy(SendGoodsPage.SGP_INPUT_COUNT), info[5].toString());
-		//车长要求
-		String carLen = "document.getElementsByName('carlength')[0].setAttribute('value','"+ info[6].toString() + "');";
+		// 车长要求
+		String carLen = "document.getElementsByName('carlength')[0].setAttribute('value','" + info[6].toString() + "');";
 		((JavascriptExecutor) seleniumUtil.driver).executeScript(carLen);
-		//车型要求
-		String carType = "document.getElementsByName('fitcartype')[0].setAttribute('value','"+ info[7].toString() + "');";
+		// 车型要求
+		String carType = "document.getElementsByName('fitcartype')[0].setAttribute('value','" + info[7].toString() + "');";
 		((JavascriptExecutor) seleniumUtil.driver).executeScript(carType);
-		//信息费
+		// 信息费
 		seleniumUtil.type(seleniumUtil.findElementBy(SendGoodsPage.SGP_INPUT_INFOFARE), info[8].toString());
-		//运费
+		// 运费
 		seleniumUtil.type(seleniumUtil.findElementBy(SendGoodsPage.SGP_INPUT_FARE), info[9].toString());
-		//声明价值
+		// 声明价值
 		seleniumUtil.type(seleniumUtil.findElementBy(SendGoodsPage.SGP_INPUT_DECLAREVALUE), info[10].toString());
-		//信息有效期
-		if(seleniumUtil.findElementBy(goodsDate).isSelected()==false){
+		// 信息有效期
+		if (seleniumUtil.findElementBy(goodsDate).isSelected() == false) {
 			seleniumUtil.click(seleniumUtil.findElementBy(goodsDate));
 		}
-		//货源说明
+		// 货源说明
 		seleniumUtil.type(seleniumUtil.findElementBy(SendGoodsPage.SGP_INPUT_INSTRUCTION), info[11].toString());
-		
+
 	}
-	/**从发布货源界面上点击按钮或者链接进入相关页面*/
+
+	/** 从发布货源界面上点击按钮或者链接进入相关页面 */
 	public static void enterPage(SeleniumUtil seleniumUtil, By elementLocator) {
 		seleniumUtil.click(seleniumUtil.findElementBy(elementLocator));
 
 	}
-	
-	/**检查发布货源失败*/
-	public static void checkSendGoodsFailedStatus(SeleniumUtil seleniumUtil){
+
+	/** 检查发布货源失败-全部必填 不填写时 */
+	public static void checkSendGoodsAllEmptyFailedStatus(SeleniumUtil seleniumUtil) {
 		logger.info("Starting checking sendgoods page error text");
 		seleniumUtil.findElementBy(SendGoodsPage.SGP_TEXT_ORIGINALCITYERROR);
 		seleniumUtil.findElementBy(SendGoodsPage.SGP_TEXT_GOODSNAMEERROR);
-		if(seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_DEVICEGOODS).isSelected()||seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_HEAVYGOODS).isSelected())
-		{
+		if (seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_DEVICEGOODS).isSelected() || seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_HEAVYGOODS).isSelected()) {
 			seleniumUtil.findElementBy(SendGoodsPage.SGP_TEXT_WEIGHTERROR);
 		}
-		if(seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_LIGHTGOODS).isSelected())
-		{
+		if (seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_LIGHTGOODS).isSelected()) {
 			seleniumUtil.findElementBy(SendGoodsPage.SGP_TEXT_VOLUMEERROR);
 		}
-	
-	
-		
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(SendGoodsPage.SGP_TEXT_ORIGINALCITYERROR).getText(), "必填字段");
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(SendGoodsPage.SGP_TEXT_GOODSNAMEERROR).getText(), "必填字段");
-//		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(SendGoodsPage.SGP_TEXT_VOLUMEERROR).getText(), "必填字段");
-//		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(SendGoodsPage.SGP_TEXT_WEIGHTERROR).getText(), "必填字段");
+		if (seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_DEVICEGOODS).isSelected() || seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_HEAVYGOODS).isSelected()) {
+			seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(SendGoodsPage.SGP_TEXT_WEIGHTERROR).getText(), "必填字段");
+		}
+		if (seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_LIGHTGOODS).isSelected()) {
+			seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(SendGoodsPage.SGP_TEXT_VOLUMEERROR).getText(), "必填字段");
+		}
+
 		logger.info("Checking sendgoods page error text complete");
+
 	}
-	
+
+	/** 检查发布货源失败-其中某一个或者某个必填项没有填写，我们只用检查，发布按钮是不是还存在，来判断是不是发布成功 */
+	public static void checkSendStatus(SeleniumUtil seleniumUtil) {
+		logger.info("Start checking sendgoods page [发布] and [清空] button are exist or not ");
+		seleniumUtil.isDisplayed(seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_SEND));
+		seleniumUtil.isDisplayed(seleniumUtil.findElementBy(SendGoodsPage.SGP_BUTTON_RESET));
+		logger.info("Checking sendgoods page [发布] and [清空] button are exist or not complete");
+	}
+
 }
