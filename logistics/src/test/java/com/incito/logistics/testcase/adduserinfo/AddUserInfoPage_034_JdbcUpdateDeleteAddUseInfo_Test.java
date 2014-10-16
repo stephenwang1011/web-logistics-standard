ccaeff79f05bb4ef24485025f95d4182f84d2640
wangkai <646651700@qq.com>
2014-10-16 11:07:11 +0800

    将更新数据库的JDBC位置放到完善信息的用例后面

--- logistics/src/test/java/com/incito/logistics/testcase/adduserinfo/AddUserInfoPage_034_JdbcUpdateDeleteAddUseInfo_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/adduserinfo/AddUserInfoPage_034_JdbcUpdateDeleteAddUseInfo_Test.java
@@ -1,10 +1,10 @@
-package com.incito.logistics.testcase.database.jdbc;
+package com.incito.logistics.testcase.adduserinfo;
 
 import org.testng.annotations.Test;
 
 import com.incito.logistics.util.Jdbc_CURD;
 
-public class JdbcUpdate extends Jdbc_CURD {
+public class AddUserInfoPage_034_JdbcUpdateDeleteAddUseInfo_Test extends Jdbc_CURD {
 	@Test
 	public void jdbcUpdate() {
 		String sql = "UPDATE smartdbtest.agent SET name = NULL, tel = NULL, icard = NULL, company = NULL, province = NULL, city = NULL, region = NULL, address = NULL, status = 1 WHERE  username = 'wangkai'";
