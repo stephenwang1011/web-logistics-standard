640e030f7de0e5f20b441fb74db94ee7fdb143a1
wangkai <646651700@qq.com>
2014-10-20 20:33:39 +0800

    添加找货源的用例的方法

--- logistics/src/test/java/com/incito/logistics/pages/FindGoodsPage.java
+++ logistics/src/test/java/com/incito/logistics/pages/FindGoodsPage.java
@@ -8,11 +8,19 @@ import org.openqa.selenium.By;
 public class FindGoodsPage {
 
 	/** 公共货源按钮 */
-	public static final By FGP_BUTTON_PUBLICDOODS = By.xpath("//*[@class='active']");
+	public static final By FGP_BUTTON_PUBLICDOODS = By.linkText("公共货源");
 	/** 我的收藏按钮 */
-	public static final By FGP_BUTTON_MYFAVORITES = By.xpath("//*[text()='我的收藏']");
+	public static final By FGP_BUTTON_MYFAVORITES = By.linkText("我的收藏");
 	/** 发货地文本框 */
 	public static final By FGP_INPUT_FROMCITY = By.id("carcity");
 	/** 收货地地文本框 */
 	public static final By FGP_INPUT_TARGETCITY = By.id("targetcity");
+	/** 车长开始输入文本框 */
+	public static final By FGP_INPUT_STARTCARLONG = By.name("startcarlong");
+	/** 车长结束输入文本框 */
+	public static final By FGP_INPUT_ENDTCARLONG = By.name("endcarlong");
+	/** 搜索按钮 */
+	public static final By FGP_BUTTON_SEARCH = By.className("btn btn-major");
+	/** 高级搜索按钮 */
+	public static final By FGP_BUTTON_ADVANCESEARCH = By.id("btn btn-major");
 }
