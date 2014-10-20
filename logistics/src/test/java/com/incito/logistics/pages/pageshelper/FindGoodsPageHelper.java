fa41e147f87da57b6d6ca5fa356b62f5d698b481
wangkai <646651700@qq.com>
2014-10-20 20:35:07 +0800

    添加找货源的方法

--- logistics/src/test/java/com/incito/logistics/pages/pageshelper/FindGoodsPageHelper.java
+++ logistics/src/test/java/com/incito/logistics/pages/pageshelper/FindGoodsPageHelper.java
@@ -1,8 +1,11 @@
 package com.incito.logistics.pages.pageshelper;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 
 import com.incito.logistics.pages.FindGoodsPage;
+import com.incito.logistics.pages.SendGoodsPage;
 import com.incito.logistics.util.SeleniumUtil;
 
 /**
@@ -13,14 +16,39 @@ import com.incito.logistics.util.SeleniumUtil;
 public class FindGoodsPageHelper {
 	public static Logger logger = Logger.getLogger(AddUserInfoPagerHelper.class.getName());
 	/** 等待找货源页面元素加载出来 */
-	public static void waitAddUserInfoPageToLoad(int timeOut, SeleniumUtil seleniumUtil) {
-		logger.info("Start checking addUserInfo page elements");
+	public static void waitFindGoodsPageToLoad(int timeOut, SeleniumUtil seleniumUtil) {
+		logger.info("Start checking FindGoods page elements");
 		seleniumUtil.waitForElementToLoad(timeOut, FindGoodsPage.FGP_BUTTON_PUBLICDOODS);
 		seleniumUtil.waitForElementToLoad(timeOut, FindGoodsPage.FGP_BUTTON_MYFAVORITES);
 		seleniumUtil.waitForElementToLoad(timeOut, FindGoodsPage.FGP_INPUT_FROMCITY);
 		seleniumUtil.waitForElementToLoad(timeOut, FindGoodsPage.FGP_INPUT_TARGETCITY);
+		seleniumUtil.waitForElementToLoad(timeOut, FindGoodsPage.FGP_INPUT_STARTCARLONG);
+		seleniumUtil.waitForElementToLoad(timeOut, FindGoodsPage.FGP_INPUT_ENDTCARLONG);
+		logger.info("Check FindGoods page elements completed");
 
-		logger.info("Check addUserInfo page elements completed");
+	}
+	
+	/**
+	 * 在查找货源上输入搜索条件
+	 * @param seleniumUtil
+	 * @param goodsType
+	 * @param goodsDate
+	 * @param info
+	 */
+	public static void typeFindGoodsInfo(SeleniumUtil seleniumUtil, String... info) {
+		// 填充发货地
+		String jsFrom = "document.getElementsByName('originalcity')[0].setAttribute('value','" + info[0].toString() + "');";
+		((JavascriptExecutor) seleniumUtil.driver).executeScript(jsFrom);
+		// 填充收货地
+		String jsTo = "document.getElementsByName('receiptcity')[0].setAttribute('value','" + info[1].toString() + "');";
+		((JavascriptExecutor) seleniumUtil.driver).executeScript(jsTo);
+
+
+	}
+	
+	/** 从找货源进入指定的页面-根据元素定位来确定什么页面 */
+	public static void enterPage(SeleniumUtil seleniumUtil, By elementLocator) {
+		seleniumUtil.click(seleniumUtil.findElementBy(elementLocator));
 
 	}
 }
