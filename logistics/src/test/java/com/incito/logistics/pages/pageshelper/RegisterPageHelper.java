f4199a34a4fd56ab977a6e91ddc9e1b1f7a538e0
wangyang <398733146@qq.com>
2014-10-22 16:28:39 +0800

    去掉网页title的检测

--- logistics/src/test/java/com/incito/logistics/pages/pageshelper/RegisterPageHelper.java
+++ logistics/src/test/java/com/incito/logistics/pages/pageshelper/RegisterPageHelper.java
@@ -26,8 +26,6 @@ public class RegisterPageHelper {
 
 	/** 检查注册页面上的文本 */
 	public static void checkRegisterPageText(SeleniumUtil seleniumUtil) {
-
-		seleniumUtil.isTextCorrect(seleniumUtil.getTitle(), "logistics - 物流管理");
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_USERNAME).getAttribute("placeholder"), "至少输入4个字符");
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_PASSWD).getAttribute("placeholder"), "至少输入6个字符");
 		seleniumUtil.isTextCorrect(seleniumUtil.findElementBy(RegisterPage.RP_INPUT_REPASSWD).getAttribute("placeholder"), "请再次输入登录密码");
