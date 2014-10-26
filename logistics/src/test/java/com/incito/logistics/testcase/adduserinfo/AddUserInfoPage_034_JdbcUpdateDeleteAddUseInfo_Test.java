e223f8074fb002515f233614f527f0c5b031df09
wangyang <398733146@qq.com>
2014-10-26 17:25:35 +0800

    引入JdbcUtil.java,代替了老的命名

--- logistics/src/test/java/com/incito/logistics/testcase/adduserinfo/AddUserInfoPage_034_JdbcUpdateDeleteAddUseInfo_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/adduserinfo/AddUserInfoPage_034_JdbcUpdateDeleteAddUseInfo_Test.java
@@ -2,9 +2,9 @@ package com.incito.logistics.testcase.adduserinfo;
 
 import org.testng.annotations.Test;
 
-import com.incito.logistics.util.Jdbc_CURD;
+import com.incito.logistics.util.JdbcUtil;
 
-public class AddUserInfoPage_034_JdbcUpdateDeleteAddUseInfo_Test extends Jdbc_CURD {
+public class AddUserInfoPage_034_JdbcUpdateDeleteAddUseInfo_Test extends JdbcUtil {
 	@Test
 	public void jdbcUpdate() {
 		String sql = "UPDATE smartdb.agent SET name = NULL, tel = NULL, icard = NULL, company = NULL, province = NULL, city = NULL, region = NULL, address = NULL, status = 1 WHERE  username = 'wangkai'";
--- logistics/src/test/java/com/incito/logistics/testcase/register/RegisterPage_012_JdbcUpdateDeleteUsername_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/register/RegisterPage_012_JdbcUpdateDeleteUsername_Test.java
@@ -2,9 +2,9 @@ package com.incito.logistics.testcase.register;
 
 import org.testng.annotations.Test;
 
-import com.incito.logistics.util.Jdbc_CURD;
+import com.incito.logistics.util.JdbcUtil;
 
-public class RegisterPage_012_JdbcUpdateDeleteUsername_Test extends Jdbc_CURD {
+public class RegisterPage_012_JdbcUpdateDeleteUsername_Test extends JdbcUtil {
 	@Test
 	public void jdbcUpdate() {
 		String sql = "DELETE FROM smartdb.agent WHERE username = 'GOGOGO'";
