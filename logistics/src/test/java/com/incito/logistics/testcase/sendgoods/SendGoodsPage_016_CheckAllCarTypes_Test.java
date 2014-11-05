bdeb19304dadaa4055daa42ae55c2491e5827c7b
wangyang <398733146@qq.com>
2014-11-05 17:51:55 +0800

    增加了一个车型：厢式车

--- logistics/src/test/java/com/incito/logistics/testcase/sendgoods/SendGoodsPage_016_CheckAllCarTypes_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/sendgoods/SendGoodsPage_016_CheckAllCarTypes_Test.java
@@ -36,7 +36,7 @@ public class SendGoodsPage_016_CheckAllCarTypes_Test extends BaseParpare {
 		SendGoodsPageHelper.enterPage(seleniumUtil, SendGoodsPage.SGP_INPUT_CARTYPE);
 		SendGoodsPageHelper.checkReserveText(seleniumUtil,timeOut, SendGoodsPage.SGP_TEXT_CARTYPE, "不限 普通车 前四后四 前四后八"+"\n"+
 											"敞篷车 平板车 高栏车 集装箱"+"\n"+"冷藏车 起重车 后八轮 单桥车"+"\n"+
-											"自卸车 半封闭车 半挂车 保温车"+"\n"+"双桥车 加长挂车 棉被车 前四后十"+"\n"+"大笼子车 高低板 翻斗车"
+											"自卸车 半封闭车 半挂车 保温车"+"\n"+"双桥车 加长挂车 棉被车 前四后十"+"\n"+"大笼子车 高低板 翻斗车 厢式车"
 				);
 
 		
