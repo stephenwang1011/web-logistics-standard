002282ec7198162a86631bf93b2df4398079b37f
wangyang <398733146@qq.com>
2014-11-12 17:39:40 +0800

    新增“找车源”测试用例

---
+++ logistics/src/test/java/com/incito/logistics/testcase/findcars/FindCarsPage_003_FromDataInputSearch_Test.java
@@ -0,0 +1,68 @@
+package com.incito.logistics.testcase.findcars;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.testng.ITestContext;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.pageshelper.FindCarsPagerHelper;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.util.ExcelDataProvider;
+import com.incito.logistics.util.PropertiesDataProvider;
+
+/**
+ * @author xy-incito-wy
+ * @Description 测试用例：找车源，所有输入出发地
+ * */
+public class FindCarsPage_003_FromDataInputSearch_Test extends BaseParpare {
+
+	@Test(dataProvider = "data")
+	public void fromDataInputSearch(ITestContext context, Map<String, String> data) {
+		String userInfoPath = context.getCurrentXmlTest().getParameter("userInfoPath");
+		String username = PropertiesDataProvider.getTestData(userInfoPath, "username");
+		String password = PropertiesDataProvider.getTestData(userInfoPath, "password");
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		int sleepTime = Integer.valueOf(context.getCurrentXmlTest().getParameter("sleepTime"));
+
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
+		LoginPageHelper.waitLoginPageToLoad(timeOut, seleniumUtil);
+		LoginPageHelper.login(seleniumUtil, username, password);
+		LoginPageHelper.checkUserInfo(timeOut, sleepTime, seleniumUtil, username);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_LINK_FINDCARS);
+		FindCarsPagerHelper.waitFindGoodsPageToLoad(timeOut, seleniumUtil);
+		FindCarsPagerHelper.checkFindGoodsPageText(timeOut, seleniumUtil);
+		FindCarsPagerHelper.typeCarsInfo(seleniumUtil, data.get("FCP_INPUT_FROM"), data.get("FCP_INPUT_TO"), 
+				data.get("FCP_INPUT_STARTCARLEN"), data.get("FCP_INPUT_ENDCARLEN"), data.get("FCP_INPUT_CARTYPE"), data.get("FCP_INPUT_STARTWEIGHT"), 
+				data.get("FCP_INPUT_ENDWEIGHT"), data.get("FCP_INPUT_STARTVOLUME"),  data.get("FCP_INPUT_ENDVOLUME"));
+		FindCarsPagerHelper.checkCurrentCarLocation(seleniumUtil, data.get("FCP_INPUT_FROM"));
+
+	}
+
+
+	@DataProvider(name = "data")
+	public Iterator<Object[]> dataFortestMethod() throws IOException {
+		String moduleName = null; // 模块的名字
+		String caseNum = null; // 用例编号
+		String className = this.getClass().getName();
+		int dotIndexNum = className.indexOf("."); // 取得第一个.的index
+		int underlineIndexNum = className.indexOf("_"); // 取得第一个_的index
+
+		if (dotIndexNum > 0) {
+			moduleName = className.substring(30, className.lastIndexOf(".")); // 取到模块的名称
+		}
+
+		if (underlineIndexNum > 0) {
+			caseNum = className.substring(underlineIndexNum + 1, underlineIndexNum + 4); // 取到用例编号
+		}
+		// 将模块名称和用例的编号传给 ExcelDataProvider ，然后进行读取excel数据
+		return new ExcelDataProvider(moduleName, caseNum);
+	}
+
+}
