46efc4af6c1575e0f073da03f243f2555a288092
wangkai <646651700@qq.com>
2014-11-12 19:09:21 +0800

    查找货源最大车长和最小车长的execl

--- logistics/src/test/java/com/incito/logistics/pages/FindGoodsPage.java
+++ logistics/src/test/java/com/incito/logistics/pages/FindGoodsPage.java
@@ -6,11 +6,13 @@ import org.openqa.selenium.By;
  * 找货源页面所有元素定位
  * */
 public class FindGoodsPage {
-
+	//********以下元素是“公共货源”和“我的收藏”的tab*************************************
 	/** 公共货源按钮 */
 	public static final By FGP_BUTTON_PUBLICDOODS = By.linkText("公共货源");
 	/** 我的收藏按钮 */
 	public static final By FGP_BUTTON_MYFAVORITES = By.linkText("我的收藏");
+	
+	//********以下元素是“搜索”选项的查找元素*************************************
 	/** 发货地文本框 */
 	public static final By FGP_INPUT_FROMCITY = By.id("carcity");
 	/** 收货地地文本框 */
@@ -24,10 +26,11 @@ public class FindGoodsPage {
 	/** 高级搜索按钮 */
 	public static final By FGP_BUTTON_ADVANCESEARCH = By.id("btn btn-major");
 	
-	/** 搜索之后的结果 */
-	public static final By FGP_DISPLAY_SEARCH = By.className("goods-row1-city");
+	//********以下元素是搜索之后的结果*******************************************
+	/** 搜索之后的第一排信息（起始地到目的地） */
+	public static final By FGP_DISPLAY_SEARCH = By.className("goods-infobox");
 	/** 搜索之后,地址下方的第二排信息*/
-	public static final By FGP_SECOND_INFO = By.cssSelector("div.goods-info-row2");
+	public static final By FGP_SECOND_INFO = By.xpath("//*[@class='goods-infobox']");
 	/** 搜索无结果提示语 */
 	public static final By FGP_NODIAPALY_SEARCH = By.xpath("//label[text()='没有搜索到相应的数据']");
 }
--- logistics/src/test/java/com/incito/logistics/pages/pageshelper/FindGoodsPageHelper.java
+++ logistics/src/test/java/com/incito/logistics/pages/pageshelper/FindGoodsPageHelper.java
@@ -6,6 +6,7 @@ import java.util.StringTokenizer;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
 
 import com.incito.logistics.pages.FindGoodsPage;
 import com.incito.logistics.util.SeleniumUtil;
@@ -58,11 +59,76 @@ public class FindGoodsPageHelper {
 
 	}
 
-	/** 检查搜索界面的是否包含发货地或者收货地搜索结果 */
-	public static void checkFindGoodsPrompt_Location(SeleniumUtil seleniumUtil, Map<String, String> data) {
-		logger.info("Start checking checkFindGoodsPrompt_Location page text");
-		seleniumUtil.isContains(seleniumUtil.findElementsBy(FindGoodsPage.FGP_DISPLAY_SEARCH).get(0).getAttribute("title"), data.get("FGP_INPUT_GOODSORIGINALCITY"));
-		logger.info("Check checkFindGoodsPrompt_Location page text completed");
+	/** 检查货源的第一行的数据：发货地和收货地 */
+	public static void checkGoodsAddress(SeleniumUtil seleniumUtil, By by, String... goodsAdd) {
+		if (seleniumUtil.findElementsBy(by).get(0).getText().equals("没有搜索到相应的数据")) {
+			logger.warn("No data found with this filters!");
+			return;
+		}
+		String address = seleniumUtil.findElementBy(by).findElement(By.xpath("//div[1]/div[1]/div[1]/span[1]")).getAttribute("title");
+		address = address.replaceAll(" ", "");
+		String add[] = address.split("至");
+		String original = add[0], targetcity = add[1];
+		if (goodsAdd[0].equals("") == false) {
+			seleniumUtil.isContains(original, goodsAdd[0]);
+		}
+		if (goodsAdd[1].equals("") == false) {
+			seleniumUtil.isContains(targetcity, goodsAdd[1]);
+		}
+	}
+
+	/** 检查货源信息的第二行信息:车长 */
+	public static void checkCarLength(SeleniumUtil seleniumUtil, By by, String... secondInfos) {
+		// 这个items指的是查询出来有多少条货源
+		int items = seleniumUtil.findElementsBy(by).size();
+		if (seleniumUtil.findElementsBy(by).get(0).getText().equals("没有搜索到相应的数据")) {
+			// 如果按照筛选条件查不到数据的时候给一个提示并退出此方法
+			logger.warn("根据您的筛选条件，无法查找出数据！");
+			return;
+		}
+
+		for (int i = 0; i < items; i++) {// 循环每个货源-只对针对当前页面的
+			double carLong = 0; // 车长
+			String second = seleniumUtil.findElementBy(by).findElements(By.xpath("//div[1]/div[1]/div[2]/span")).get(i).getText(); // 取得第二行的货源信息
+			String[] secondArray = second.split("，");
+			for (String temp : secondArray) {
+				if (temp.trim().contains("车辆要求")) {
+					String[] temp1 = temp.split("：");
+					carLong = Double.parseDouble(temp1[1].replaceAll("米", ""));// 取得车长
+
+					if (secondInfos[1].equals("") && secondInfos[0] != "") {
+						try {
+							Assert.assertTrue(carLong >= Double.parseDouble(secondInfos[0]));
+						} catch (AssertionError e) {
+							logger.error("Found the length of car in web page is [" + carLong + "] and is less than input length num [" + secondInfos[0] + "] T");
+							Assert.fail("Found the weight of car in web page is [" + carLong + "] and is less than input length num [" + secondInfos[0] + "] T");
+						}
+						logger.info("The length of the " + (i + 1) + "the goods'car info is [" + carLong + "] T");
+					}
+
+					if (secondInfos[0].equals("") && secondInfos[1] != "") {
+						try {
+							Assert.assertTrue(carLong <= Double.parseDouble(secondInfos[1]));
+						} catch (AssertionError e) {
+							logger.error("Found the length of car in web page is [" + carLong + "] and is longer than input length num [" + secondInfos[1] + "] T");
+							Assert.fail("Found the weight in web page is [" + carLong + "] and is longer than input length num [" + secondInfos[1] + "] T");
+						}
+						logger.info("The length of the " + (i + 1) + "the goods'car info is [" + carLong + "] T");
+					}
+
+					if (secondInfos[0] != "" && secondInfos[1] != "") {
+						try {
+							Assert.assertTrue(Double.parseDouble(secondInfos[1]) <= carLong && carLong >= Double.parseDouble(secondInfos[1]));
+						} catch (AssertionError e) {
+							logger.error("Found the length of car in web page is [" + carLong + "] and is not in input length num [" + secondInfos[0] + "] and [" + secondInfos[1] + "] T");
+							Assert.fail("Found the weight in web page is [" + carLong + "] and is not in input length num [" + secondInfos[0] + "] and [" + secondInfos[1] + "] T");
+						}
+						logger.info("The length of the " + (i + 1) + "the goods'car info is [" + carLong + "] T");
+					}
+				}
+			}
+		}
+
 	}
 
 	/** 检查搜索界面的是否包含搜索结果 */
