e2de1b00d5cc8d5402b0a23c409a32922132f758
wangyang <398733146@qq.com>
2014-11-14 11:19:24 +0800

    新增

---
+++ logistics/src/test/java/com/incito/logistics/testcase/home/HomePage_013_NotLogin_QuickSearchCars_Test.java
@@ -0,0 +1,31 @@
+package com.incito.logistics.testcase.home;
+
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.LoginPage;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+/**
+ *@author  xy-incito-wy
+ *@Description 测试用例：未登录情况下点击查询 弹出登陆页面
+ *
+ * */
+public class HomePage_013_NotLogin_QuickSearchCars_Test extends BaseParpare {
+	@Test
+	public void quickSearchCars(ITestContext context) {
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_LINK_FINDCARS, 1);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_SEARCH2);
+		LoginPageHelper.waitLoginPageToLoad(timeOut, seleniumUtil);
+		LoginPageHelper.checkLoginPageText(seleniumUtil);
+		LoginPageHelper.enterPage(seleniumUtil, LoginPage.LP_BUTTON_CLOSE);
+
+	}
+
+}
