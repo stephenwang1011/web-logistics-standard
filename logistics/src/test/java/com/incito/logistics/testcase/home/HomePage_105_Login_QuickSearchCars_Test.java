ac8e83f24e5f6312d533e22b5bec7e336b96e823
wangyang <398733146@qq.com>
2014-11-14 17:56:45 +0800

    未完成的用例

---
+++ logistics/src/test/java/com/incito/logistics/testcase/home/HomePage_105_Login_QuickSearchCars_Test.java
@@ -0,0 +1,41 @@
+package com.incito.logistics.testcase.home;
+
+import java.util.Map;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.pageshelper.FindCarsPageHelper;
+import com.incito.logistics.pages.pageshelper.FindGoodsPageHelper;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.pages.pageshelper.MyOrdersPageHelper;
+import com.incito.logistics.util.PropertiesDataProvider;
+
+/**
+ * @author xy-incito-wy
+ * @Description 登录情况下 进行快速查找车源操作，只输入出发地
+ * 
+ * */
+public class HomePage_105_Login_QuickSearchCars_Test extends BaseParpare {
+	@Test(dataProvider="data")
+	public void login_QuickSearchCars(ITestContext context,Map<String,String> data) {
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		String configFilePath = String.valueOf(context.getCurrentXmlTest().getParameter("userInfoPath"));
+		String username = PropertiesDataProvider.getTestData(configFilePath, "username");
+		String password = PropertiesDataProvider.getTestData(configFilePath, "password");
+		int sleepTime = Integer.valueOf(context.getCurrentXmlTest().getParameter("sleepTime"));
+
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.checkHomePageText(seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
+		LoginPageHelper.login(seleniumUtil, username, password);
+		HomePageHelper.holdOn(seleniumUtil, sleepTime);
+		
+
+
+
+	}
+}
