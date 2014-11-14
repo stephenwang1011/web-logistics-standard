c9681d3380348e6f0a735d7f9fe5b9d375ab3e63
wangyang <398733146@qq.com>
2014-11-14 17:20:28 +0800

    我的订单UI检查

---
+++ logistics/src/test/java/com/incito/logistics/testcase/myorders/MyOrdersPage_001_UITextCheck_Test.java
@@ -0,0 +1,40 @@
+package com.incito.logistics.testcase.myorders;
+
+/**
+ *@author  xy-incito-wy
+ *@Description 测试用例：我的订单UI、文本测试
+ *
+ * */
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.pageshelper.FooterPageHelper;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.pages.pageshelper.MyOrdersPageHelper;
+import com.incito.logistics.util.PropertiesDataProvider;
+
+public class MyOrdersPage_001_UITextCheck_Test extends BaseParpare {
+	@Test
+	public void myOrdersUiTest(ITestContext context) {
+		String userInfoPath = context.getCurrentXmlTest().getParameter("userInfoPath");
+		String username = PropertiesDataProvider.getTestData(userInfoPath, "username");
+		String password = PropertiesDataProvider.getTestData(userInfoPath, "password");
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		int sleepTime = Integer.valueOf(context.getCurrentXmlTest().getParameter("sleepTime"));
+		
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
+		LoginPageHelper.waitLoginPageToLoad(timeOut, seleniumUtil);
+		LoginPageHelper.login(seleniumUtil, username, password);
+		HomePageHelper.holdOn(seleniumUtil, sleepTime);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_LINK_MYORDER);
+		MyOrdersPageHelper.waitForMyOrdersPageToLoad(seleniumUtil, timeOut);
+		MyOrdersPageHelper.checkMyOrdersPageText(seleniumUtil);
+		FooterPageHelper.checkFooterPageText(seleniumUtil);
+
+	}
+
+}
