12703efcae22ab7fc325fd0936ae85b89d0e2689
wangyang <398733146@qq.com>
2014-11-15 17:13:05 +0800

    完善了此用例

--- logistics/src/test/java/com/incito/logistics/testcase/home/HomePage_105_Login_QuickSearchCars_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/home/HomePage_105_Login_QuickSearchCars_Test.java
@@ -1,17 +1,19 @@
 package com.incito.logistics.testcase.home;
 
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.testng.ITestContext;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.incito.logistics.base.BaseParpare;
 import com.incito.logistics.pages.HomePage;
 import com.incito.logistics.pages.pageshelper.FindCarsPageHelper;
-import com.incito.logistics.pages.pageshelper.FindGoodsPageHelper;
 import com.incito.logistics.pages.pageshelper.HomePageHelper;
 import com.incito.logistics.pages.pageshelper.LoginPageHelper;
-import com.incito.logistics.pages.pageshelper.MyOrdersPageHelper;
+import com.incito.logistics.util.ExcelDataProvider;
 import com.incito.logistics.util.PropertiesDataProvider;
 
 /**
@@ -33,9 +35,30 @@ public class HomePage_105_Login_QuickSearchCars_Test extends BaseParpare {
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
 		LoginPageHelper.login(seleniumUtil, username, password);
 		HomePageHelper.holdOn(seleniumUtil, sleepTime);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_LINK_FINDCARS, 1);
 		
+		HomePageHelper.typeQuickSearchInfo(seleniumUtil, HomePage.HP_LINK_FINDCARS, data.get("FROM"),data.get("TO"),data.get("NO"));
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_SEARCH2);
+		FindCarsPageHelper.waitFindCarsPageToLoad(timeOut, seleniumUtil);
+		FindCarsPageHelper.checkCurrentCarLocation(seleniumUtil, data.get("FROM"));
 
+	}
+	@DataProvider(name = "data")
+	public Iterator<Object[]> dataFortestMethod() throws IOException {
+		String moduleName = null; // 模块的名字
+		String caseNum = null; // 用例编号
+		String className = this.getClass().getName();
+		int dotIndexNum = className.indexOf("."); // 取得第一个.的index
+		int underlineIndexNum = className.indexOf("_"); // 取得第一个_的index
 
+		if (dotIndexNum > 0) {
+			moduleName = className.substring(30, className.lastIndexOf(".")); // 取到模块的名称
+		}
 
+		if (underlineIndexNum > 0) {
+			caseNum = className.substring(underlineIndexNum + 1, underlineIndexNum + 4); // 取到用例编号
+		}
+		// 将模块名称和用例的编号传给 ExcelDataProvider ，然后进行读取excel数据
+		return new ExcelDataProvider(moduleName, caseNum);
 	}
 }
