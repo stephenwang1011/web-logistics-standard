bb180dd1db8a7b7108fbc70ad4fee959dc1a9726
wangyang <398733146@qq.com>
2014-11-17 21:13:24 +0800

    完成了此用例

--- logistics/src/test/java/com/incito/logistics/testcase/mygoods/MyGoodsPage_016_EditGoods_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/mygoods/MyGoodsPage_016_EditGoods_Test.java
@@ -50,7 +50,10 @@ public class MyGoodsPage_016_EditGoods_Test extends BaseParpare {
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_LINK_MYGOODS);
 		MyGoodsPageHelper.waitMyGoodsPageToLoad(timeOut, seleniumUtil);
 		MyGoodsPageHelper.editTargetGoods(seleniumUtil, data.get("INSTRUCTION"));
-
+		SendGoodsPageHelper.checkInput(seleniumUtil, SendGoodsPage.SGP_BUTTON_LIGHTGOODS, SendGoodsPage.SGP_BUTTON_GOODSDATE5, 
+				data.get("SGP_INPUT_GOODSORIGINALCITY"), data.get("SGP_INPUT_GOODSRECEIPTCITY"), data.get("SGP_INPUT_GOODSNAME"), data.get("SGP_INPUT_GOODSDETAILS"),
+				data.get("SGP_INPUT_VOLUME"), data.get("SGP_INPUT_COUNT"), data.get("SGP_INPUT_CARLENGTH"), data.get("SGP_INPUT_CARTYPE"), 
+				data.get("SGP_INPUT_INFOFARE"), data.get("SGP_INPUT_FARE"), data.get("SGP_INPUT_DECLAREVALUE"), data.get("SGP_INPUT_INSTRUCTION"));
 
 		
 		
