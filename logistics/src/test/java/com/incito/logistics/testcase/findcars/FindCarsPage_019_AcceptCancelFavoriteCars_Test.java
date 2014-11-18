647af9f8c2d1497691945fc168c3894d025d2bea
wangyang <398733146@qq.com>
2014-11-18 14:27:19 +0800

    用例重新命名

--- logistics/src/test/java/com/incito/logistics/testcase/findcars/FindCarsPage_019_AcceptCancelFavoriteCars_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/findcars/FindCarsPage_019_AcceptCancelFavoriteCars_Test.java
@@ -21,10 +21,10 @@ import com.incito.logistics.util.PropertiesDataProvider;
  * @author xy-incito-wy
  * @Description 测试用例：收藏的车源，在我的车源列表中取消收藏车源，会弹出确认取消收藏的提示框
  * */
-public class FindCarsPage_019_CancelFavoriteCars_Test extends BaseParpare {
+public class FindCarsPage_019_AcceptCancelFavoriteCars_Test extends BaseParpare {
 
 	@Test(dataProvider = "data")
-	public void cancelFavoriteCars(ITestContext context, Map<String, String> data) {
+	public void acceptCancelFavoriteCars(ITestContext context, Map<String, String> data) {
 		String userInfoPath = context.getCurrentXmlTest().getParameter("userInfoPath");
 		String username = PropertiesDataProvider.getTestData(userInfoPath, "username");
 		String password = PropertiesDataProvider.getTestData(userInfoPath, "password");
