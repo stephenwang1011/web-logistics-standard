afc228a777013fadfd49980550cba8a4feb9157d
wangyang <398733146@qq.com>
2014-11-25 14:00:49 +0800

    1、新增首页4条用例 2、新增清单数据

---
+++ logistics/src/test/java/com/incito/logistics/testcase/home/HomePage_113_Login_MoreCars_Test.java
@@ -0,0 +1,39 @@
+package com.incito.logistics.testcase.home;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.pageshelper.FindCarsPageHelper;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.util.PropertiesDataProvider;
+
+	/**
+	 * @author xy-incito-wy
+	 * @Description 登录情况下点击公共车源上的 "更多>>" 进入找车源页面
+	 * 
+	 * */
+	public class HomePage_113_Login_MoreCars_Test extends BaseParpare {
+	@Test
+	public void clickMoreCars(ITestContext context) {
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		int sleepTime = Integer.valueOf(context.getCurrentXmlTest().getParameter("sleepTime"));
+		String configFilePath = String.valueOf(context.getCurrentXmlTest().getParameter("userInfoPath"));
+		String username = PropertiesDataProvider.getTestData(configFilePath, "username");
+		String password = PropertiesDataProvider.getTestData(configFilePath, "password");
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.checkHomePageText(seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
+		LoginPageHelper.login(seleniumUtil, username, password);
+		HomePageHelper.holdOn(seleniumUtil, sleepTime);
+
+		seleniumUtil.click(seleniumUtil.findElementsBy(HomePage.HP_LINK_MORE).get(0));
+		FindCarsPageHelper.waitFindCarsPageToLoad(timeOut, seleniumUtil);
+
+		}
+
+	}
+
+
