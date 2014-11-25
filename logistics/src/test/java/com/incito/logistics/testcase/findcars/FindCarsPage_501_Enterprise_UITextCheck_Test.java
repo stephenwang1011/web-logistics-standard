7e2fab81b11d2a69a525bf27894e930140bd635c
wangyang <398733146@qq.com>
2014-11-25 17:43:23 +0800

    新增测试用例：企业版的 找车源相关

---
+++ logistics/src/test/java/com/incito/logistics/testcase/findcars/FindCarsPage_501_Enterprise_UITextCheck_Test.java
@@ -0,0 +1,40 @@
+package com.incito.logistics.testcase.findcars;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.pageshelper.FindCarsPageHelper;
+import com.incito.logistics.pages.pageshelper.FooterPageHelper;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.util.PropertiesDataProvider;
+
+/**
+ * @author xy-incito-wy
+ * @Description 企业账户下的 - 找车源，UI文本检查
+ * */
+public class FindCarsPage_501_Enterprise_UITextCheck_Test extends BaseParpare {
+
+	@Test
+	public void enterpriseFindCarsPageUiTextCheck(ITestContext context) {
+		String userInfoPath = context.getCurrentXmlTest().getParameter("userInfoPath");
+		String enterprise_username = PropertiesDataProvider.getTestData(userInfoPath, "enterprise_username");
+		String enterprise_password = PropertiesDataProvider.getTestData(userInfoPath, "enterprise_password");
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		int sleepTime = Integer.valueOf(context.getCurrentXmlTest().getParameter("sleepTime"));
+
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
+		LoginPageHelper.waitLoginPageToLoad(timeOut, seleniumUtil);
+		LoginPageHelper.login(seleniumUtil, enterprise_username, enterprise_password);
+		HomePageHelper.holdOn(seleniumUtil, sleepTime);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_LINK_FINDCARS);
+		FindCarsPageHelper.waitFindCarsPageToLoad(timeOut, seleniumUtil);
+		FindCarsPageHelper.checkFindCarsPageText(timeOut, seleniumUtil);
+		FooterPageHelper.checkFooterPageText(seleniumUtil);
+
+	}
+
+}
