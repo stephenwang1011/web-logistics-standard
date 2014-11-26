11bb93a1f263476ae8a5eb73a67730e54b211e5a
wangkai <646651700@qq.com>
2014-11-26 17:31:07 +0800

    添加企业版本的我的订单用例

---
+++ logistics/src/test/java/com/incito/logistics/testcase/myorders/MyOrdersPage_701_Enterprise_PulicCarsOrders_UITextCheck_Test.java
@@ -0,0 +1,42 @@
+package com.incito.logistics.testcase.myorders;
+
+/**
+ *@author  xy-incito-wk
+ *@Description 测试用例：我的订单UI、文本测试
+ *
+ * */
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.MyOrdersPage;
+import com.incito.logistics.pages.pageshelper.FooterPageHelper;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.pages.pageshelper.MyOrdersPageHelper;
+import com.incito.logistics.util.PropertiesDataProvider;
+
+public class MyOrdersPage_701_Enterprise_PulicCarsOrders_UITextCheck_Test extends BaseParpare {
+	@Test
+	public void myOrdersUiTest(ITestContext context) {
+		String userInfoPath = context.getCurrentXmlTest().getParameter("userInfoPath");
+		String username = PropertiesDataProvider.getTestData(userInfoPath, "enterprise_username");
+		String password = PropertiesDataProvider.getTestData(userInfoPath, "enterprise_password");
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		int sleepTime = Integer.valueOf(context.getCurrentXmlTest().getParameter("sleepTime"));
+		
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
+		LoginPageHelper.waitLoginPageToLoad(timeOut, seleniumUtil);
+		LoginPageHelper.login(seleniumUtil, username, password);
+		HomePageHelper.holdOn(seleniumUtil, sleepTime);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_LINK_MYORDER);
+		MyOrdersPageHelper.waitForMyOrdersPageToLoad(seleniumUtil, timeOut);
+		
+		MyOrdersPageHelper.enterPage(seleniumUtil, MyOrdersPage.MOP_TAB_SOCIETYCARS);
+		MyOrdersPageHelper.waitForMyOrdersPageToLoad(seleniumUtil, timeOut);
+		MyOrdersPageHelper.checkMyOrdersPageTextForSocietyCarsOrders(seleniumUtil);
+		FooterPageHelper.checkFooterPageText(seleniumUtil);
+	}
+}
