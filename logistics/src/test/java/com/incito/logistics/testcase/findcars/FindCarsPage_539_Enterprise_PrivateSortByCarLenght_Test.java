833a055101724996d587532493e9bfa35f58e750
wangyang <398733146@qq.com>
2014-11-26 17:02:30 +0800

    新增企业版-我的车源相关用例

---
+++ logistics/src/test/java/com/incito/logistics/testcase/findcars/FindCarsPage_539_Enterprise_PrivateSortByCarLenght_Test.java
@@ -0,0 +1,41 @@
+package com.incito.logistics.testcase.findcars;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.FindCarsPage;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.pageshelper.FindCarsPageHelper;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.util.PropertiesDataProvider;
+
+/**
+ * @author xy-incito-wy
+ * @Description 按照信用等级排序车源
+ * */
+public class FindCarsPage_539_Enterprise_PrivateSortByCarLenght_Test extends BaseParpare {
+
+	@Test
+	public void privateSortByCarLenForEnterprise(ITestContext context) {
+		String userInfoPath = context.getCurrentXmlTest().getParameter("userInfoPath");
+		String username = PropertiesDataProvider.getTestData(userInfoPath, "enterprise_username");
+		String password = PropertiesDataProvider.getTestData(userInfoPath, "enterprise_password");
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		int sleepTime = Integer.valueOf(context.getCurrentXmlTest().getParameter("sleepTime"));
+
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
+		LoginPageHelper.waitLoginPageToLoad(timeOut, seleniumUtil);
+		LoginPageHelper.login(seleniumUtil, username, password);
+		LoginPageHelper.checkUserInfo(timeOut, sleepTime, seleniumUtil, username);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_LINK_FINDCARS);
+		FindCarsPageHelper.waitFindCarsPageToLoad(timeOut, seleniumUtil);
+		FindCarsPageHelper.enterPage(seleniumUtil, FindCarsPage.FCP_TAB_FAV);
+		FindCarsPageHelper.checkCarsSort(seleniumUtil, "车长", timeOut);
+
+
+	}
+
+}
