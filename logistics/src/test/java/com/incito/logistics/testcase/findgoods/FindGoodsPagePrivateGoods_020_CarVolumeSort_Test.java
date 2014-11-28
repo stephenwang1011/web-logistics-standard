87da5e902a048eb975e04cb595729da65926a096
wangkai <646651700@qq.com>
2014-11-28 17:16:42 +0800

    标准版的我的收藏的排序

---
+++ logistics/src/test/java/com/incito/logistics/testcase/findgoods/FindGoodsPagePrivateGoods_020_CarVolumeSort_Test.java
@@ -0,0 +1,39 @@
+package com.incito.logistics.testcase.findgoods;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.FindGoodsPage;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.pageshelper.FindGoodsPageHelper;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.util.PropertiesDataProvider;
+
+/**
+ * @author xy-incito-wk
+ * @Description 找货源-公共货源上，默认排序检查
+ * */
+public class FindGoodsPagePrivateGoods_020_CarVolumeSort_Test extends BaseParpare {
+	@Test
+	public void carVolumeSort(ITestContext context) {
+		String configFilePath = String.valueOf(context.getCurrentXmlTest().getParameter("userInfoPath"));
+		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		int sleepTime = Integer.valueOf(context.getCurrentXmlTest().getParameter("sleepTime"));
+		String username = PropertiesDataProvider.getTestData(configFilePath, "registered_username");
+		String password = PropertiesDataProvider.getTestData(configFilePath, "registered_password");
+
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
+		LoginPageHelper.waitLoginPageToLoad(timeOut, seleniumUtil);
+		LoginPageHelper.login(seleniumUtil, username, password);
+		LoginPageHelper.checkUserInfo(timeOut, sleepTime, seleniumUtil, username);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_LINK_FINDGOODS);
+		FindGoodsPageHelper.waitFindGoodsPageToLoad(timeOut, seleniumUtil);
+		FindGoodsPageHelper.enterPage(seleniumUtil, FindGoodsPage.FGP_BUTTON_MYFAVORITES);
+		FindGoodsPageHelper.waitFindGoodsPageToLoad(timeOut, seleniumUtil);
+		
+		FindGoodsPageHelper.checkCarsSort(seleniumUtil, "体积", timeOut);
+	}
+}
