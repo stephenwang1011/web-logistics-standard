43e6883f7a1e300b08b9301899259dd078a310a0
wangyang <398733146@qq.com>
2014-12-16 10:47:19 +0800

    新方法

---
+++ logistics/src/test/java/com/incito/logistics/plugins/father/FlowFather.java
@@ -0,0 +1,32 @@
+package com.incito.logistics.plugins.father;
+
+import org.testng.ITestContext;
+
+import com.incito.logistics.base.BaseParpare;
+import com.incito.logistics.pages.HomePage;
+import com.incito.logistics.pages.pageshelper.HomePageHelper;
+import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.util.PropertiesDataProvider;
+import com.incito.logistics.util.SeleniumUtil;
+
+public class FlowFather extends BaseParpare {
+	protected static int timeOut = 0;
+	protected static int sleepTime = 0;
+
+	public static void flowParpare(ITestContext context, SeleniumUtil seleniumUtil) {
+
+		String userInfoPath = context.getCurrentXmlTest().getParameter("userInfoPath");
+		String username = PropertiesDataProvider.getTestData(userInfoPath, "username");
+		String password = PropertiesDataProvider.getTestData(userInfoPath, "password");
+		timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
+		sleepTime = Integer.valueOf(context.getCurrentXmlTest().getParameter("sleepTime"));
+
+		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
+		LoginPageHelper.waitLoginPageToLoad(timeOut, seleniumUtil);
+		LoginPageHelper.login(seleniumUtil, username, password);
+		LoginPageHelper.checkUserInfo(timeOut, sleepTime, seleniumUtil, username);
+
+	}
+
+}
