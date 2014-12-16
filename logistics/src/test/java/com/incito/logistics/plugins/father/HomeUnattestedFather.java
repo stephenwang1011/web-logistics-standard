cbb75c7e1c692b0a4a2d6a710c5ffdf02464cb40
wangyang <398733146@qq.com>
2014-12-16 12:04:17 +0800

    修改名字

--- logistics/src/test/java/com/incito/logistics/plugins/father/HomeUnattestedFather.java
+++ logistics/src/test/java/com/incito/logistics/plugins/father/HomeUnattestedFather.java
@@ -14,7 +14,7 @@ import com.incito.logistics.util.SeleniumUtil;
  * @description 每个流程 开始之间的一个准备类，主要是做登录准备以及相关数据加载准备
  * 
  * */
-public class HomeUnauthenticationFather extends BaseParpare {
+public class HomeUnattestedFather extends BaseParpare {
 	protected static int timeOut = 0;
 	protected static int sleepTime = 0;
 
--- logistics/src/test/java/com/incito/logistics/testcase/home/HomePage_118_Login_UnattestedUserGoodsFav_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/home/HomePage_118_Login_UnattestedUserGoodsFav_Test.java
@@ -6,17 +6,17 @@ import org.testng.ITestContext;
 import org.testng.annotations.Test;
 
 import com.incito.logistics.pages.pageshelper.HomePageHelper;
-import com.incito.logistics.plugins.father.HomeUnauthenticationFather;
+import com.incito.logistics.plugins.father.HomeUnattestedFather;
 
 /**
  * @author xy-incito-wy
  * @Description 登录情况下，未认证的用户去点击收藏按钮，应该收藏成功并且在找车源-我的收藏中有刚才收藏的车源
  * 
  * */
-public class HomePage_118_Login_UnattestedUserGoodsFav_Test extends HomeUnauthenticationFather {
+public class HomePage_118_Login_UnattestedUserGoodsFav_Test extends HomeUnattestedFather {
 	@Test(dataProvider="data")
 	public void unauthenticationUserGoodsFav(ITestContext context,Map<String,String> data) {
-		HomeUnauthenticationFather.homeUnauthenticationParpare(context, seleniumUtil);
+		HomeUnattestedFather.homeUnauthenticationParpare(context, seleniumUtil);
 		HomePageHelper.clickFavButtonByLicenseAndDriver(data.get("License"),data.get("Driver"), seleniumUtil);
 
 	}
