aa435f6f7fcc93bc63f72f5f4c8bda8a2210f791
wangyang <398733146@qq.com>
2014-12-29 16:09:38 +0800

    代码优化  架构改进

--- logistics/src/test/java/com/incito/logistics/testcase/home/HomePage_014_NotLogin_QuickSearchGoods_Test.java
+++ logistics/src/test/java/com/incito/logistics/testcase/home/HomePage_014_NotLogin_QuickSearchGoods_Test.java
@@ -4,22 +4,21 @@ package com.incito.logistics.testcase.home;
 import org.testng.ITestContext;
 import org.testng.annotations.Test;
 
-import com.incito.logistics.base.BaseParpare;
 import com.incito.logistics.pages.HomePage;
 import com.incito.logistics.pages.LoginPage;
 import com.incito.logistics.pages.pageshelper.HomePageHelper;
 import com.incito.logistics.pages.pageshelper.LoginPageHelper;
+import com.incito.logistics.plugins.father.HomeNotLoginFather;
 /**
  *@author  xy-incito-wy
  *@Description 测试用例：未登录情况下点击查询 弹出登陆页面
  *
  * */
-public class HomePage_014_NotLogin_QuickSearchGoods_Test extends BaseParpare {
+public class HomePage_014_NotLogin_QuickSearchGoods_Test extends HomeNotLoginFather {
 	@Test
 	public void quickSearchCars(ITestContext context) {
-		int timeOut = Integer.valueOf(context.getCurrentXmlTest().getParameter("timeOut"));
-		
-		HomePageHelper.waitHomePageToLoad(timeOut, seleniumUtil);
+		HomeNotLoginFather.homeNotLoginParpare(context, seleniumUtil);
+	
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_QUICK_FINDGOODS);
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_SEARCH3);
 		LoginPageHelper.waitLoginPageToLoad(timeOut, seleniumUtil);
