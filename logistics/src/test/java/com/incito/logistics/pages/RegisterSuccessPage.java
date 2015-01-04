96311fb205527e2bc1e5d851e6f5f503dff8b5e9
wangyang <398733146@qq.com>
2015-01-04 15:25:29 +0800

    注册成功页面上的元素新增

---
+++ logistics/src/test/java/com/incito/logistics/pages/RegisterSuccessPage.java
@@ -0,0 +1,19 @@
+package com.incito.logistics.pages;
+
+import org.openqa.selenium.By;
+
+/**
+ * 注册成功页面元素声明
+ * 
+ * */
+public class RegisterSuccessPage {
+	/** 恭喜注册成功 title */
+	public static final By RSP_TEXT_SUCCESSTITLE = By.className("in-text-primary");
+	/**注册成功相关提示信息*/
+	public static final By RSP_TEXT_PROMPT = By.className("register-prompt");
+	/**认证个人信息button*/
+	public static final By RSP_BUTTON_ATTESTINFO = By.xpath("//*[text()='认证个人信息']");
+	/**去首页button*/		
+	public static final By RSP_BUTTON_GOHOME = By.xpath("//*[text()='去首页']");
+
+}
