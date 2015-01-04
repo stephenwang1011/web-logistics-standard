16645f8bea0ab2dba59c3e4a7e29eaf57f473577
wangyang <398733146@qq.com>
2015-01-04 15:03:39 +0800

    新增协议页面

---
+++ logistics/src/test/java/com/incito/logistics/pages/AgreementPage.java
@@ -0,0 +1,21 @@
+package com.incito.logistics.pages;
+
+import org.openqa.selenium.By;
+/**
+ * 
+ * @author wangyang
+ * @Description 协议页面上的元素声明
+ *
+ */
+public class AgreementPage {
+	/**协议title */
+	public static final By AP_TEXT_TITLE = By.className("register-agreement-title");
+	/**协议content */
+	public static final By AP_TEXT_CONTENT = By.className("register-agreement-content");
+	/**关闭按钮*/
+	public static final By AP_BUTTON_CLOSE = By.id("close_");
+	
+	
+
+
+}
