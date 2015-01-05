a8654d674f1c737f9804cf04bdafd0c9eeed7873
wangkai <646651700@qq.com>
2015-01-05 18:52:58 +0800

    添加基本信息中已认证的用户信息

---
+++ logistics/src/test/java/com/incito/logistics/testcase/userBaseInfo/UserBaseInfoPage_005_Authenticated_QQ_Test.java
@@ -0,0 +1,31 @@
+package com.incito.logistics.testcase.userBaseInfo;
+
+import java.util.Map;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.pages.UserBaseInfoPage;
+import com.incito.logistics.pages.pageshelper.UserBaseInfoPagerHelper;
+import com.incito.logistics.plugins.father.UserBaseInfoFather;
+
+/**
+ * @author xy-incito-wk
+ * @Description 填写电话，身份证号，公司名称，公司地址点击提交
+ *
+ * */
+public class UserBaseInfoPage_005_Authenticated_QQ_Test extends UserBaseInfoFather {
+	@Test(dataProvider = "data")
+	public void userBaseInfoPageFailTelIdcardCompanyAddress(ITestContext context, Map<String, String> data) {
+		UserBaseInfoFather.userBaseInfoParpare(context, seleniumUtil, true);
+
+		UserBaseInfoPagerHelper.typeUserBaseInfo(seleniumUtil, 
+				data.get("BUIP_TEXT_SEX"), data.get("BUIP_INPUT_BIRTHDAY"),
+				data.get("BUIP_INPUT_QQ"), data.get("BUIP_INPUT_EMAIL"));
+		UserBaseInfoPagerHelper.saveOrCancel(seleniumUtil, UserBaseInfoPage.BUIP_BUTTON_SAVE, true);
+		seleniumUtil.waitForElementToLoad(timeOut, UserBaseInfoPage.UBIP_TITLE_USERINFO);
+		UserBaseInfoPagerHelper.checkUserBaseInfo(seleniumUtil, 
+				data.get("BUIP_TEXT_SEX"), data.get("BUIP_INPUT_BIRTHDAY"),
+				data.get("BUIP_INPUT_QQ"), data.get("BUIP_INPUT_EMAIL"));
+	}
+}
