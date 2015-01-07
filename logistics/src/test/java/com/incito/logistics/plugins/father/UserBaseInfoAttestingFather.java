800ccb06a6194f77c48177f1cdd7ace432139b4e
wangyang <398733146@qq.com>
2015-01-07 15:35:50 +0800

    修改了状态定位

--- logistics/src/test/java/com/incito/logistics/plugins/father/UserBaseInfoAttestingFather.java
+++ logistics/src/test/java/com/incito/logistics/plugins/father/UserBaseInfoAttestingFather.java
@@ -33,7 +33,7 @@ public class UserBaseInfoAttestingFather extends BaseParpare {
 		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_BUTTON_LOGIN);
 		LoginPageHelper.login(seleniumUtil, attestingUserName, attestingPassword);
 		HomePageHelper.holdOn(seleniumUtil, sleepTime);
-		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_LINK_YESAUTHOR);
+		HomePageHelper.enterPage(seleniumUtil, HomePage.HP_LINK_NOAUTHOR);
 		UserBaseInfoPagerHelper.enterPage(seleniumUtil, UserBaseInfoPage.UBIP_TAB_BASEINFO);
 		if (flag) {
 			UserBaseInfoPagerHelper.enterPage(seleniumUtil, UserBaseInfoPage.BUIP_BUTTON_REVISE);
