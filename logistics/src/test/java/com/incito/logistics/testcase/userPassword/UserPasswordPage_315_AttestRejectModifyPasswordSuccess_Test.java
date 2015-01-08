c9ffea57b987aab08e4bfe24bbbc39055a692d22
wangyang <398733146@qq.com>
2015-01-08 21:23:44 +0800

    新增认证驳回的用例

---
+++ logistics/src/test/java/com/incito/logistics/testcase/userPassword/UserPasswordPage_315_AttestRejectModifyPasswordSuccess_Test.java
@@ -0,0 +1,30 @@
+package com.incito.logistics.testcase.userPassword;
+
+import java.util.Map;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import com.incito.logistics.pages.UserPasswordPage;
+import com.incito.logistics.pages.pageshelper.UserPasswordPageHelper;
+import com.incito.logistics.plugins.father.UserPasswordAttestRejectFather;
+/**
+ * 
+ * @author xy-incito-wy
+ * @Decription 认证驳回的用户登录之后，进入修改密码页面，前密码为空，新密码和确认密码不为空并且相同，点击提交按钮，不能修改提交成功
+ */
+public class UserPasswordPage_315_AttestRejectModifyPasswordSuccess_Test extends UserPasswordAttestRejectFather{
+	
+	@Test(dataProvider="data")
+	public void attestRejectModifyPasswordSuccess(ITestContext context,Map<String,String> data){
+		UserPasswordAttestRejectFather.userPasswordParpare(context, seleniumUtil);
+		UserPasswordPageHelper.typeChangePasswdInfo(seleniumUtil, data.get("OLD_PASSWORD"), data.get("NEW_PASSWORD"), data.get("CORFIRM_PASSWORD"));
+		UserPasswordPageHelper.enterPage(seleniumUtil, UserPasswordPage.USP_BUTTON_OK);
+		UserPasswordPageHelper.checkModifyPasswdSuccessed(seleniumUtil, sleepTime, timeOut, modifyusername3, data.get("NEW_PASSWORD"));
+
+	}
+	
+	
+	
+
+}
